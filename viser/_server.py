aeb5f5cc601832f162e23b960fe0e1ad0ca766bf
Brent Yi <yibrenth@gmail.com>
2023-03-14 22:40:42 -0700

    Polish multi-panel

--- viser/_server.py
+++ viser/_server.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-
 import asyncio
-import multiprocessing
-import queue
+import dataclasses
+import queue as queue_
 import threading
 import time
-from typing import Optional
+from typing import Dict, List, Literal, Optional, Union
 
 import msgpack
 import websockets.connection
@@ -18,6 +17,13 @@ from ._async_message_buffer import AsyncMessageBuffer
 from ._messages import Message, ViewerCameraMessage
 
 
+@dataclasses.dataclass
+class Connection:
+    camera: Optional[ViewerCameraMessage]
+    camera_timestamp: float
+    _message_buffer: asyncio.Queue
+
+
 class ViserServer:
     def __init__(
         self,
@@ -25,36 +31,55 @@ class ViserServer:
         port: int = 8080,
     ):
         # Create websocket server process.
-        self.message_queue = queue.Queue(maxsize=1024)
-        self.camera: Optional[ViewerCameraMessage] = None
-        self.camera_timestamp: float = 0.0
+        self.connection_from_id: Dict[int, Connection] = {}
+        self._message_queue = queue_.Queue(maxsize=1024)
         threading.Thread(
             target=self._start_background_loop,
-            args=(host, port, self.message_queue),
+            args=(host, port, self._message_queue),
             daemon=True,
         ).start()
 
-    def queue(self, *messages: Message) -> None:
-        """Queue a message, which will be sent to all clients."""
+    def queue(
+        self,
+        *messages: Message,
+        connection_id: Union[Literal["broadcast"], int] = "broadcast",
+    ) -> None:
+        """Queue a message, which can either be broadcast or sent to a particular connection ID."""
         for message in messages:
-            self.message_queue.put_nowait(message)
+            self._message_queue.put_nowait((connection_id, message))
 
     def _start_background_loop(
         self,
         host: str,
         port: int,
-        message_queue: multiprocessing.Queue,
+        message_queue: queue_.Queue,
     ) -> None:
         connection_count = 0
         total_connections = 0
         event_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(event_loop)
-        message_buffer = AsyncMessageBuffer(event_loop)
+
+        # We maintain two message buffer types: a persistent broadcasted messages and
+        # connection-specific messages.
+        broadcast_buffer = AsyncMessageBuffer(event_loop)
 
         def message_transfer() -> None:
             """Message transfer loop. Pulls messages from the main process and pushes them into our buffer."""
             while True:
-                message_buffer.push(message_queue.get())
+                connection_id, message = message_queue.get()
+                if connection_id == "broadcast":
+                    broadcast_buffer.push(message)
+                elif connection_id in self.connection_from_id:
+                    event_loop.call_soon_threadsafe(
+                        self.connection_from_id[
+                            connection_id
+                        ]._message_buffer.put_nowait,
+                        message,
+                    )
+                else:
+                    print(
+                        f"Tried to send message to {connection_id}, but ID is closed or not valid."
+                    )
 
         async def serve(websocket: WebSocketServerProtocol) -> None:
             """Server loop, run once per connection."""
@@ -67,14 +92,19 @@ class ViserServer:
             nonlocal total_connections
             total_connections += 1
 
+            single_connection_buffer = asyncio.Queue()
+            connection = Connection(None, 0.0, single_connection_buffer)
+            self.connection_from_id[connection_id] = connection
+
             print(
                 f"Connection opened ({connection_id}, {total_connections} total),"
-                f" {len(message_buffer.message_from_id)} buffered messages"
+                f" {len(broadcast_buffer.message_from_id)} buffered messages"
             )
             try:
                 await asyncio.gather(
-                    producer(websocket),
-                    consumer(websocket),
+                    single_connection_producer(websocket, single_connection_buffer),
+                    broadcast_producer(websocket),
+                    consumer(websocket, connection),
                 )
             except (
                 websockets.exceptions.ConnectionClosedOK,
@@ -82,19 +112,30 @@ class ViserServer:
             ):
                 total_connections -= 1
                 print(f"Connection closed ({connection_id}, {total_connections} total)")
+                self.connection_from_id.pop(connection_id)
 
-        async def producer(websocket: WebSocketServerProtocol) -> None:
+        async def single_connection_producer(
+            websocket: WebSocketServerProtocol, buffer: asyncio.Queue
+        ) -> None:
             # Infinite loop to send messages from the message buffer.
-            async for message in message_buffer:
-                await websocket.send(message)
+            while True:
+                message = await buffer.get()
+                await websocket.send(message.serialize())
 
-        async def consumer(websocket: WebSocketServerProtocol) -> None:
+        async def broadcast_producer(websocket: WebSocketServerProtocol) -> None:
+            # Infinite loop to send messages from the message buffer.
+            async for message_serialized in broadcast_buffer:
+                await websocket.send(message_serialized)
+
+        async def consumer(
+            websocket: WebSocketServerProtocol, connection: Connection
+        ) -> None:
             while True:
                 message = msgpack.unpackb(await websocket.recv())
                 t = message.pop("type")
                 if t == "viewer_camera":
-                    self.camera = ViewerCameraMessage(**message)
-                    self.camera_timestamp = time.time()
+                    connection.camera = ViewerCameraMessage(**message)
+                    connection.camera_timestamp = time.time()
                 else:
                     print("Unrecognized message", message)
 
