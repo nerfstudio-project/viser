22bae607b2daec917872b3a08877b5d12e80430c
Hang Gao <hangg.sv7@gmail.com>
2023-03-24 19:53:17 -0700

    Demonstrating nerf example with nerfacc

---
+++ examples/8_nerfacc/datasets/nerf_360_v2.py
@@ -0,0 +1,364 @@
+"""
+Copyright (c) 2022 Ruilong Li, UC Berkeley.
+"""
+
+import collections
+import os
+import sys
+
+import imageio
+import numpy as np
+import torch
+import torch.nn.functional as F
+import tqdm
+
+from .utils import Rays
+
+_PATH = os.path.abspath(__file__)
+
+sys.path.insert(
+    0, os.path.join(os.path.dirname(_PATH), "..", "pycolmap", "pycolmap")
+)
+from scene_manager import SceneManager
+
+
+def _load_colmap(root_fp: str, subject_id: str, factor: int = 1):
+    assert factor in [1, 2, 4, 8]
+
+    data_dir = os.path.join(root_fp, subject_id)
+    colmap_dir = os.path.join(data_dir, "sparse/0/")
+
+    manager = SceneManager(colmap_dir)
+    manager.load_cameras()
+    manager.load_images()
+
+    # Assume shared intrinsics between all cameras.
+    cam = manager.cameras[1]
+    fx, fy, cx, cy = cam.fx, cam.fy, cam.cx, cam.cy
+    K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+    K[:2, :] /= factor
+
+    # Extract extrinsic matrices in world-to-camera format.
+    imdata = manager.images
+    w2c_mats = []
+    bottom = np.array([0, 0, 0, 1]).reshape(1, 4)
+    for k in imdata:
+        im = imdata[k]
+        rot = im.R()
+        trans = im.tvec.reshape(3, 1)
+        w2c = np.concatenate([np.concatenate([rot, trans], 1), bottom], axis=0)
+        w2c_mats.append(w2c)
+    w2c_mats = np.stack(w2c_mats, axis=0)
+
+    # Convert extrinsics to camera-to-world.
+    camtoworlds = np.linalg.inv(w2c_mats)
+
+    # Image names from COLMAP. No need for permuting the poses according to
+    # image names anymore.
+    image_names = [imdata[k].name for k in imdata]
+
+    # # Switch from COLMAP (right, down, fwd) to Nerf (right, up, back) frame.
+    # poses = poses @ np.diag([1, -1, -1, 1])
+
+    # Get distortion parameters.
+    type_ = cam.camera_type
+
+    if type_ == 0 or type_ == "SIMPLE_PINHOLE":
+        params = None
+        camtype = "perspective"
+
+    elif type_ == 1 or type_ == "PINHOLE":
+        params = None
+        camtype = "perspective"
+
+    if type_ == 2 or type_ == "SIMPLE_RADIAL":
+        params = {k: 0.0 for k in ["k1", "k2", "k3", "p1", "p2"]}
+        params["k1"] = cam.k1
+        camtype = "perspective"
+
+    elif type_ == 3 or type_ == "RADIAL":
+        params = {k: 0.0 for k in ["k1", "k2", "k3", "p1", "p2"]}
+        params["k1"] = cam.k1
+        params["k2"] = cam.k2
+        camtype = "perspective"
+
+    elif type_ == 4 or type_ == "OPENCV":
+        params = {k: 0.0 for k in ["k1", "k2", "k3", "p1", "p2"]}
+        params["k1"] = cam.k1
+        params["k2"] = cam.k2
+        params["p1"] = cam.p1
+        params["p2"] = cam.p2
+        camtype = "perspective"
+
+    elif type_ == 5 or type_ == "OPENCV_FISHEYE":
+        params = {k: 0.0 for k in ["k1", "k2", "k3", "k4"]}
+        params["k1"] = cam.k1
+        params["k2"] = cam.k2
+        params["k3"] = cam.k3
+        params["k4"] = cam.k4
+        camtype = "fisheye"
+
+    assert params is None, "Only support pinhole camera model."
+
+    # Previous Nerf results were generated with images sorted by filename,
+    # ensure metrics are reported on the same test set.
+    inds = np.argsort(image_names)
+    image_names = [image_names[i] for i in inds]
+    camtoworlds = camtoworlds[inds]
+
+    # Load images.
+    if factor > 1:
+        image_dir_suffix = f"_{factor}"
+    else:
+        image_dir_suffix = ""
+    colmap_image_dir = os.path.join(data_dir, "images")
+    image_dir = os.path.join(data_dir, "images" + image_dir_suffix)
+    for d in [image_dir, colmap_image_dir]:
+        if not os.path.exists(d):
+            raise ValueError(f"Image folder {d} does not exist.")
+    # Downsampled images may have different names vs images used for COLMAP,
+    # so we need to map between the two sorted lists of files.
+    colmap_files = sorted(os.listdir(colmap_image_dir))
+    image_files = sorted(os.listdir(image_dir))
+    colmap_to_image = dict(zip(colmap_files, image_files))
+    image_paths = [
+        os.path.join(image_dir, colmap_to_image[f]) for f in image_names
+    ]
+    print("loading images")
+    images = [imageio.imread(x) for x in tqdm.tqdm(image_paths)]
+    images = np.stack(images, axis=0)
+
+    # Select the split.
+    all_indices = np.arange(images.shape[0])
+    split_indices = {
+        "test": all_indices[all_indices % 8 == 0],
+        "train": all_indices[all_indices % 8 != 0],
+    }
+    return images, camtoworlds, K, split_indices
+
+
+def similarity_from_cameras(c2w, strict_scaling):
+    """
+    reference: nerf-factory
+    Get a similarity transform to normalize dataset
+    from c2w (OpenCV convention) cameras
+    :param c2w: (N, 4)
+    :return T (4,4) , scale (float)
+    """
+    t = c2w[:, :3, 3]
+    R = c2w[:, :3, :3]
+
+    # (1) Rotate the world so that z+ is the up axis
+    # we estimate the up axis by averaging the camera up axes
+    ups = np.sum(R * np.array([0, -1.0, 0]), axis=-1)
+    world_up = np.mean(ups, axis=0)
+    world_up /= np.linalg.norm(world_up)
+
+    up_camspace = np.array([0.0, -1.0, 0.0])
+    c = (up_camspace * world_up).sum()
+    cross = np.cross(world_up, up_camspace)
+    skew = np.array(
+        [
+            [0.0, -cross[2], cross[1]],
+            [cross[2], 0.0, -cross[0]],
+            [-cross[1], cross[0], 0.0],
+        ]
+    )
+    if c > -1:
+        R_align = np.eye(3) + skew + (skew @ skew) * 1 / (1 + c)
+    else:
+        # In the unlikely case the original data has y+ up axis,
+        # rotate 180-deg about x axis
+        R_align = np.array([[-1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+
+    #  R_align = np.eye(3) # DEBUG
+    R = R_align @ R
+    fwds = np.sum(R * np.array([0, 0.0, 1.0]), axis=-1)
+    t = (R_align @ t[..., None])[..., 0]
+
+    # (2) Recenter the scene using camera center rays
+    # find the closest point to the origin for each camera's center ray
+    nearest = t + (fwds * -t).sum(-1)[:, None] * fwds
+
+    # median for more robustness
+    translate = -np.median(nearest, axis=0)
+
+    #  translate = -np.mean(t, axis=0)  # DEBUG
+
+    transform = np.eye(4)
+    transform[:3, 3] = translate
+    transform[:3, :3] = R_align
+
+    # (3) Rescale the scene using camera distances
+    scale_fn = np.max if strict_scaling else np.median
+    scale = 1.0 / scale_fn(np.linalg.norm(t + translate, axis=-1))
+
+    return transform, scale
+
+
+class SubjectLoader(torch.utils.data.Dataset):
+    """Single subject data loader for training and evaluation."""
+
+    SPLITS = ["train", "test"]
+    SUBJECT_IDS = [
+        "garden",
+        "bicycle",
+        "bonsai",
+        "counter",
+        "kitchen",
+        "room",
+        "stump",
+    ]
+
+    OPENGL_CAMERA = False
+
+    def __init__(
+        self,
+        subject_id: str,
+        root_fp: str,
+        split: str,
+        color_bkgd_aug: str = "white",
+        num_rays: int = None,
+        near: float = None,
+        far: float = None,
+        batch_over_images: bool = True,
+        factor: int = 1,
+        device: str = "cpu",
+    ):
+        super().__init__()
+        assert split in self.SPLITS, "%s" % split
+        assert subject_id in self.SUBJECT_IDS, "%s" % subject_id
+        assert color_bkgd_aug in ["white", "black", "random"]
+        self.split = split
+        self.num_rays = num_rays
+        self.near = near
+        self.far = far
+        self.training = (num_rays is not None) and (
+            split in ["train", "trainval"]
+        )
+        self.color_bkgd_aug = color_bkgd_aug
+        self.batch_over_images = batch_over_images
+        self.images, self.camtoworlds, self.K, split_indices = _load_colmap(
+            root_fp, subject_id, factor
+        )
+        # normalize the scene
+        T, sscale = similarity_from_cameras(
+            self.camtoworlds, strict_scaling=False
+        )
+        self.camtoworlds = np.einsum("nij, ki -> nkj", self.camtoworlds, T)
+        self.camtoworlds[:, :3, 3] *= sscale
+        # split
+        indices = split_indices[split]
+        self.images = self.images[indices]
+        self.camtoworlds = self.camtoworlds[indices]
+        # to tensor
+        self.images = torch.from_numpy(self.images).to(torch.uint8).to(device)
+        self.camtoworlds = (
+            torch.from_numpy(self.camtoworlds).to(torch.float32).to(device)
+        )
+        self.K = torch.tensor(self.K).to(torch.float32).to(device)
+        self.height, self.width = self.images.shape[1:3]
+
+    def __len__(self):
+        return len(self.images)
+
+    @torch.no_grad()
+    def __getitem__(self, index):
+        data = self.fetch_data(index)
+        data = self.preprocess(data)
+        return data
+
+    def preprocess(self, data):
+        """Process the fetched / cached data with randomness."""
+        pixels, rays = data["rgb"], data["rays"]
+
+        if self.training:
+            if self.color_bkgd_aug == "random":
+                color_bkgd = torch.rand(3, device=self.images.device)
+            elif self.color_bkgd_aug == "white":
+                color_bkgd = torch.ones(3, device=self.images.device)
+            elif self.color_bkgd_aug == "black":
+                color_bkgd = torch.zeros(3, device=self.images.device)
+        else:
+            # just use white during inference
+            color_bkgd = torch.ones(3, device=self.images.device)
+
+        return {
+            "pixels": pixels,  # [n_rays, 3] or [h, w, 3]
+            "rays": rays,  # [n_rays,] or [h, w]
+            "color_bkgd": color_bkgd,  # [3,]
+            **{k: v for k, v in data.items() if k not in ["rgb", "rays"]},
+        }
+
+    def update_num_rays(self, num_rays):
+        self.num_rays = num_rays
+
+    def fetch_data(self, index):
+        """Fetch the data (it maybe cached for multiple batches)."""
+        num_rays = self.num_rays
+
+        if self.training:
+            if self.batch_over_images:
+                image_id = torch.randint(
+                    0,
+                    len(self.images),
+                    size=(num_rays,),
+                    device=self.images.device,
+                )
+            else:
+                image_id = [index]
+            x = torch.randint(
+                0, self.width, size=(num_rays,), device=self.images.device
+            )
+            y = torch.randint(
+                0, self.height, size=(num_rays,), device=self.images.device
+            )
+        else:
+            image_id = [index]
+            x, y = torch.meshgrid(
+                torch.arange(self.width, device=self.images.device),
+                torch.arange(self.height, device=self.images.device),
+                indexing="xy",
+            )
+            x = x.flatten()
+            y = y.flatten()
+
+        # generate rays
+        rgb = self.images[image_id, y, x] / 255.0  # (num_rays, 3)
+        c2w = self.camtoworlds[image_id]  # (num_rays, 3, 4)
+        camera_dirs = F.pad(
+            torch.stack(
+                [
+                    (x - self.K[0, 2] + 0.5) / self.K[0, 0],
+                    (y - self.K[1, 2] + 0.5)
+                    / self.K[1, 1]
+                    * (-1.0 if self.OPENGL_CAMERA else 1.0),
+                ],
+                dim=-1,
+            ),
+            (0, 1),
+            value=(-1.0 if self.OPENGL_CAMERA else 1.0),
+        )  # [num_rays, 3]
+
+        # [num_rays, 3]
+        directions = (camera_dirs[:, None, :] * c2w[:, :3, :3]).sum(dim=-1)
+        origins = torch.broadcast_to(c2w[:, :3, -1], directions.shape)
+        viewdirs = directions / torch.linalg.norm(
+            directions, dim=-1, keepdims=True
+        )
+
+        if self.training:
+            origins = torch.reshape(origins, (num_rays, 3))
+            viewdirs = torch.reshape(viewdirs, (num_rays, 3))
+            rgb = torch.reshape(rgb, (num_rays, 3))
+        else:
+            origins = torch.reshape(origins, (self.height, self.width, 3))
+            viewdirs = torch.reshape(viewdirs, (self.height, self.width, 3))
+            rgb = torch.reshape(rgb, (self.height, self.width, 3))
+
+        rays = Rays(origins=origins, viewdirs=viewdirs)
+
+        return {
+            "rgb": rgb,  # [h, w, 3] or [num_rays, 3]
+            "rays": rays,  # [h, w, 3] or [num_rays, 3]
+        }
---
+++ examples/8_nerfacc/radiance_fields/ngp.py
@@ -0,0 +1,279 @@
+"""
+Copyright (c) 2022 Ruilong Li, UC Berkeley.
+"""
+
+from typing import Callable, List, Union
+
+import numpy as np
+import torch
+from torch.autograd import Function
+from torch.cuda.amp import custom_bwd, custom_fwd
+
+try:
+    import tinycudann as tcnn
+except ImportError as e:
+    print(
+        f"Error: {e}! "
+        "Please install tinycudann by: "
+        "pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch"
+    )
+    exit()
+
+
+class _TruncExp(Function):  # pylint: disable=abstract-method
+    # Implementation from torch-ngp:
+    # https://github.com/ashawkey/torch-ngp/blob/93b08a0d4ec1cc6e69d85df7f0acdfb99603b628/activation.py
+    @staticmethod
+    @custom_fwd(cast_inputs=torch.float32)
+    def forward(ctx, x):  # pylint: disable=arguments-differ
+        ctx.save_for_backward(x)
+        return torch.exp(x)
+
+    @staticmethod
+    @custom_bwd
+    def backward(ctx, g):  # pylint: disable=arguments-differ
+        x = ctx.saved_tensors[0]
+        return g * torch.exp(torch.clamp(x, max=15))
+
+
+trunc_exp = _TruncExp.apply
+
+
+def contract_to_unisphere(
+    x: torch.Tensor,
+    aabb: torch.Tensor,
+    ord: Union[str, int] = 2,
+    #  ord: Union[float, int] = float("inf"),
+    eps: float = 1e-6,
+    derivative: bool = False,
+):
+    aabb_min, aabb_max = torch.split(aabb, 3, dim=-1)
+    x = (x - aabb_min) / (aabb_max - aabb_min)
+    x = x * 2 - 1  # aabb is at [-1, 1]
+    mag = torch.linalg.norm(x, ord=ord, dim=-1, keepdim=True)
+    mask = mag.squeeze(-1) > 1
+
+    if derivative:
+        dev = (2 * mag - 1) / mag**2 + 2 * x**2 * (
+            1 / mag**3 - (2 * mag - 1) / mag**4
+        )
+        dev[~mask] = 1.0
+        dev = torch.clamp(dev, min=eps)
+        return dev
+    else:
+        x[mask] = (2 - 1 / mag[mask]) * (x[mask] / mag[mask])
+        x = x / 4 + 0.5  # [-inf, inf] is at [0, 1]
+        return x
+
+
+class NGPRadianceField(torch.nn.Module):
+    """Instance-NGP Radiance Field"""
+
+    def __init__(
+        self,
+        aabb: Union[torch.Tensor, List[float]],
+        num_dim: int = 3,
+        use_viewdirs: bool = True,
+        density_activation: Callable = lambda x: trunc_exp(x - 1),
+        unbounded: bool = False,
+        base_resolution: int = 16,
+        max_resolution: int = 4096,
+        geo_feat_dim: int = 15,
+        n_levels: int = 16,
+        log2_hashmap_size: int = 19,
+    ) -> None:
+        super().__init__()
+        if not isinstance(aabb, torch.Tensor):
+            aabb = torch.tensor(aabb, dtype=torch.float32)
+        self.register_buffer("aabb", aabb)
+        self.num_dim = num_dim
+        self.use_viewdirs = use_viewdirs
+        self.density_activation = density_activation
+        self.unbounded = unbounded
+        self.base_resolution = base_resolution
+        self.max_resolution = max_resolution
+        self.geo_feat_dim = geo_feat_dim
+        self.n_levels = n_levels
+        self.log2_hashmap_size = log2_hashmap_size
+
+        per_level_scale = np.exp(
+            (np.log(max_resolution) - np.log(base_resolution)) / (n_levels - 1)
+        ).tolist()
+
+        if self.use_viewdirs:
+            self.direction_encoding = tcnn.Encoding(
+                n_input_dims=num_dim,
+                encoding_config={
+                    "otype": "Composite",
+                    "nested": [
+                        {
+                            "n_dims_to_encode": 3,
+                            "otype": "SphericalHarmonics",
+                            "degree": 4,
+                        },
+                        # {"otype": "Identity", "n_bins": 4, "degree": 4},
+                    ],
+                },
+            )
+
+        self.mlp_base = tcnn.NetworkWithInputEncoding(
+            n_input_dims=num_dim,
+            n_output_dims=1 + self.geo_feat_dim,
+            encoding_config={
+                "otype": "HashGrid",
+                "n_levels": n_levels,
+                "n_features_per_level": 2,
+                "log2_hashmap_size": log2_hashmap_size,
+                "base_resolution": base_resolution,
+                "per_level_scale": per_level_scale,
+            },
+            network_config={
+                "otype": "FullyFusedMLP",
+                "activation": "ReLU",
+                "output_activation": "None",
+                "n_neurons": 64,
+                "n_hidden_layers": 1,
+            },
+        )
+        if self.geo_feat_dim > 0:
+            self.mlp_head = tcnn.Network(
+                n_input_dims=(
+                    (
+                        self.direction_encoding.n_output_dims
+                        if self.use_viewdirs
+                        else 0
+                    )
+                    + self.geo_feat_dim
+                ),
+                n_output_dims=3,
+                network_config={
+                    "otype": "FullyFusedMLP",
+                    "activation": "ReLU",
+                    "output_activation": "None",
+                    "n_neurons": 64,
+                    "n_hidden_layers": 2,
+                },
+            )
+
+    def query_density(self, x, return_feat: bool = False):
+        if self.unbounded:
+            x = contract_to_unisphere(x, self.aabb)
+        else:
+            aabb_min, aabb_max = torch.split(self.aabb, self.num_dim, dim=-1)
+            x = (x - aabb_min) / (aabb_max - aabb_min)
+        selector = ((x > 0.0) & (x < 1.0)).all(dim=-1)
+        x = (
+            self.mlp_base(x.view(-1, self.num_dim))
+            .view(list(x.shape[:-1]) + [1 + self.geo_feat_dim])
+            .to(x)
+        )
+        density_before_activation, base_mlp_out = torch.split(
+            x, [1, self.geo_feat_dim], dim=-1
+        )
+        density = (
+            self.density_activation(density_before_activation)
+            * selector[..., None]
+        )
+        if return_feat:
+            return density, base_mlp_out
+        else:
+            return density
+
+    def _query_rgb(self, dir, embedding, apply_act: bool = True):
+        # tcnn requires directions in the range [0, 1]
+        if self.use_viewdirs:
+            dir = (dir + 1.0) / 2.0
+            d = self.direction_encoding(dir.reshape(-1, dir.shape[-1]))
+            h = torch.cat([d, embedding.reshape(-1, self.geo_feat_dim)], dim=-1)
+        else:
+            h = embedding.reshape(-1, self.geo_feat_dim)
+        rgb = (
+            self.mlp_head(h)
+            .reshape(list(embedding.shape[:-1]) + [3])
+            .to(embedding)
+        )
+        if apply_act:
+            rgb = torch.sigmoid(rgb)
+        return rgb
+
+    def forward(
+        self,
+        positions: torch.Tensor,
+        directions: torch.Tensor = None,
+    ):
+        if self.use_viewdirs and (directions is not None):
+            assert (
+                positions.shape == directions.shape
+            ), f"{positions.shape} v.s. {directions.shape}"
+            density, embedding = self.query_density(positions, return_feat=True)
+            rgb = self._query_rgb(directions, embedding=embedding)
+        return rgb, density  # type: ignore
+
+
+class NGPDensityField(torch.nn.Module):
+    """Instance-NGP Density Field used for resampling"""
+
+    def __init__(
+        self,
+        aabb: Union[torch.Tensor, List[float]],
+        num_dim: int = 3,
+        density_activation: Callable = lambda x: trunc_exp(x - 1),
+        unbounded: bool = False,
+        base_resolution: int = 16,
+        max_resolution: int = 128,
+        n_levels: int = 5,
+        log2_hashmap_size: int = 17,
+    ) -> None:
+        super().__init__()
+        if not isinstance(aabb, torch.Tensor):
+            aabb = torch.tensor(aabb, dtype=torch.float32)
+        self.register_buffer("aabb", aabb)
+        self.num_dim = num_dim
+        self.density_activation = density_activation
+        self.unbounded = unbounded
+        self.base_resolution = base_resolution
+        self.max_resolution = max_resolution
+        self.n_levels = n_levels
+        self.log2_hashmap_size = log2_hashmap_size
+
+        per_level_scale = np.exp(
+            (np.log(max_resolution) - np.log(base_resolution)) / (n_levels - 1)
+        ).tolist()
+
+        self.mlp_base = tcnn.NetworkWithInputEncoding(
+            n_input_dims=num_dim,
+            n_output_dims=1,
+            encoding_config={
+                "otype": "HashGrid",
+                "n_levels": n_levels,
+                "n_features_per_level": 2,
+                "log2_hashmap_size": log2_hashmap_size,
+                "base_resolution": base_resolution,
+                "per_level_scale": per_level_scale,
+            },
+            network_config={
+                "otype": "FullyFusedMLP",
+                "activation": "ReLU",
+                "output_activation": "None",
+                "n_neurons": 64,
+                "n_hidden_layers": 1,
+            },
+        )
+
+    def forward(self, positions: torch.Tensor):
+        if self.unbounded:
+            positions = contract_to_unisphere(positions, self.aabb)
+        else:
+            aabb_min, aabb_max = torch.split(self.aabb, self.num_dim, dim=-1)
+            positions = (positions - aabb_min) / (aabb_max - aabb_min)
+        selector = ((positions > 0.0) & (positions < 1.0)).all(dim=-1)
+        density_before_activation = (
+            self.mlp_base(positions.view(-1, self.num_dim))
+            .view(list(positions.shape[:-1]) + [1])
+            .to(positions)
+        )
+        density = (
+            self.density_activation(density_before_activation)
+            * selector[..., None]
+        )
+        return density
