1e656e206b05f4d1cc477085e83f9cbc0fb0cba9
Jonah Bedouch <jonah@bedouch.net>
2023-05-30 20:35:04 -0700

    Implement button, image, and padding insertion without backend
    Co-authored by: Abhik Ahuja <ahujaabhik@berkeley.edu>

--- viser/client/src/Titlebar.tsx
+++ viser/client/src/Titlebar.tsx
@@ -1,10 +1,107 @@
 import Box from "@mui/material/Box";
-import { Grid } from "@mui/material";
+import { Button, Grid, Icon, SvgIcon } from "@mui/material";
+import { Server } from "ws";
 
-export function Titlebar() {
-    const useTitlebar = true;
+const buttonVariants = ['text', 'contained', 'outlined'];
+function isVariant(variant: unknown): variant is ('text' | 'contained' | 'outlined') {
+    return typeof variant === 'string' && buttonVariants.includes(variant);
+}
 
-    if (!useTitlebar) {
+interface TitlebarButtonProps {
+    text?: string;
+    href?: string;
+    icon?: string;
+    variant?: string;
+}
+function isButtonData(obj: any): obj is TitlebarButtonProps {
+    return (
+        typeof obj === "object" &&
+        (obj.text === undefined || typeof obj.text === "string") &&
+        (obj.href === undefined || typeof obj.href === "string") &&
+        (obj.icon === undefined || typeof obj.icon === "string") &&
+        (obj.variant === undefined || typeof obj.variant === "string")
+    );
+}
+
+interface TitlebarImageProps {
+    imageSource: string;
+    alt: string;
+}
+function isImageData(obj: any): obj is TitlebarImageProps {
+    return (
+        typeof obj === "object" &&
+        typeof obj.imageSource === "string" &&
+        typeof obj.alt === "string"
+    );
+}
+
+interface TitlebarPaddingProps {
+    width: string;
+}
+function isPaddingData(obj: any): obj is TitlebarPaddingProps {
+    return (
+        typeof obj === "object" &&
+        typeof obj.width == "string"
+    );
+}
+
+export function TitlebarButton(props: TitlebarButtonProps) {
+    return (
+        <Button
+            variant={isVariant(props.variant) ? props.variant : 'contained'}
+            href={props.href ?? ''}
+            sx={{
+                marginY: '0.4em',
+                marginX: '0.125em',
+                alignItems: 'center'
+            }}
+            size="small"
+            target="_blank"
+        >
+            {props.icon !== undefined ? (<Icon style={{marginRight: '0.2em', fontSize: '1em'}}>{props.icon}</Icon>) : null}
+            <span style={{lineHeight: '1', marginTop: '0.25em'}}>{props.text ?? ''}</span>
+        </Button>
+    )
+}
+
+/**
+ * 
+ * @param props src is a data url or a link to an image
+ * @returns image
+ */
+export function TitlebarImage(props: TitlebarImageProps) {
+    return (
+        <img src={props.imageSource} alt={props.alt} style={{ height: "2em", marginLeft: '0.125em', marginRight: '0.125em' }} />
+    )
+}
+
+
+export function TitlebarPadding(props: TitlebarPaddingProps) {
+    return (
+        <div style={{ width: props.width, height: '100%' }}></div>
+    )
+}
+
+function renderTitlebarObject(object: TitlebarButtonProps | TitlebarImageProps | TitlebarPaddingProps, index: number) {
+    if (isImageData(object)) {
+        return (<TitlebarImage key={index} imageSource={object.imageSource} alt={object.alt} />);
+    }
+    if (isPaddingData(object)) {
+        return (<TitlebarPadding key={index} width={object.width} />);
+    }
+    if (isButtonData(object)) {
+        return (<TitlebarButton key={index} text={object.text} icon={object.icon} href={object.href} variant={object.variant} />);
+    }
+    return null;
+}
+
+export function Titlebar(props: { useTitlebar: boolean; }) {
+
+    const left: Array<TitlebarButtonProps | TitlebarImageProps | TitlebarPaddingProps> = [{ text: "Getting Started", href: "https://docs.nerf.studio/en/latest/quickstart/viewer_quickstart.html", variant: "outlined" }, { text: "Github", icon: "github", href: "https://github.com/nerfstudio-project/nerfstudio", variant: "outlined" }, { text: "Documentation", icon: "description", href: "https://docs.nerf.studio/", variant: "outlined" }, { text: "Viewport Controls", icon: "keyboard", href: "https://viewer.nerf.studio/", variant: "outlined" }]
+    const center: Array<TitlebarButtonProps | TitlebarImageProps | TitlebarPaddingProps> = []
+    const right: Array<TitlebarButtonProps | TitlebarImageProps | TitlebarPaddingProps> = [{ imageSource: "https://docs.nerf.studio/en/latest/_images/logo.png", alt: "Nerfstudio Logo" }, {width: '5em'}]
+
+    if (!props.useTitlebar) {
         return null;
     }
 
@@ -13,7 +110,6 @@ export function Titlebar() {
             container
             sx={{
                 width: "100%",
-                height: "2.5em",
                 zIndex: "1000",
                 backgroundColor: "rgba(255, 255, 255, 0.85)",
                 borderBottom: "1px solid",
@@ -21,18 +117,40 @@ export function Titlebar() {
                 direction: "row",
                 justifyContent: "space-between",
                 alignItems: "center",
-                paddingX: "1em"
+                paddingX: "0.875em"
             }}
         >
-                <Box component="div">
-                    title
-                </Box>
-                <Box component="div">
-                    title
-                </Box>
-                <Box component="div">
-                    title
-                </Box>
+            <Grid item xs="auto" component="div"
+                sx={{
+                    display: "flex",
+                    direction: "row",
+                    alignItems: "center",
+                    justifyContent: "left",
+                    overflow: 'visible'
+                }}
+            >
+                {left.map((object, index) => renderTitlebarObject(object, index))}
+            </Grid>
+            <Grid item xs component="div"
+                sx={{
+                    display: "flex",
+                    direction: "row",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }} 
+            >
+                {center.map((object, index) => renderTitlebarObject(object, index))}
+            </Grid>
+            <Grid item xs={4} component="div"
+                sx={{
+                    display: "flex",
+                    direction: "row",
+                    alignItems: "center",
+                    justifyContent: "right"
+                }}
+            >
+                {right.map((object, index) => renderTitlebarObject(object, index))}
+            </Grid>
         </Grid>
     );
 }
