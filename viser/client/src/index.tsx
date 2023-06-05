e6a059212e5b72677636ff6e1c042dbe0fa30ffa
Brent Yi <yibrenth@gmail.com>
2023-06-05 14:55:31 -0700

    Remove multi-panel support, bottom bar (#44)
    * Remove multi-panel support, bottom bar
    
    * Fix search params

--- viser/client/src/index.tsx
+++ viser/client/src/index.tsx
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
-import { Environment } from "@react-three/drei";
+import { CameraControls, Environment } from "@react-three/drei";
 import * as THREE from "three";
 import { Canvas, useThree } from "@react-three/fiber";
 
-import { CameraPrimitives, SynchronizedCameraControls } from "./CameraControls";
-import React, { MutableRefObject, useContext, useRef, useState } from "react";
+import { SynchronizedCameraControls } from "./CameraControls";
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 import ControlPanel from "./ControlPanel/ControlPanel";
@@ -18,42 +18,27 @@ import {
 import "./index.css";
 
 import Box from "@mui/material/Box";
-import {
-  FormControlLabel,
-  IconButton,
-  useMediaQuery,
-  Grid,
-  Switch,
-} from "@mui/material";
-import { RemoveCircleRounded, AddCircleRounded } from "@mui/icons-material";
 import WebsocketInterface from "./WebsocketInterface";
 import { UseGui, useGuiState } from "./ControlPanel/GuiState";
-import {
-  getServersFromSearchParams,
-  searchParamKey,
-  truncateSearchParamServers,
-} from "./SearchParamsUtils";
+import { searchParamKey } from "./SearchParamsUtils";
 
 type ViewerContextContents = {
-  panelKey: number;
   useSceneTree: UseSceneTree;
   useGui: UseGui;
-  websocketRef: MutableRefObject<WebSocket | null>;
+  websocketRef: React.MutableRefObject<WebSocket | null>;
   wrapperRef: React.RefObject<HTMLDivElement>;
-  globalCameras: MutableRefObject<CameraPrimitives>;
   objFromSceneNodeNameRef: React.MutableRefObject<{
     [name: string]: THREE.Object3D | undefined;
   }>;
   sceneRef: React.MutableRefObject<THREE.Scene | null>;
+  cameraRef: React.MutableRefObject<THREE.PerspectiveCamera | null>;
+  cameraControlRef: React.MutableRefObject<CameraControls | null>;
 };
 export const ViewerContext = React.createContext<null | ViewerContextContents>(
   null
 );
 
-const SingleViewer = React.memo(function SingleViewer(props: {
-  panelKey: number;
-  globalCameras: MutableRefObject<CameraPrimitives>;
-}) {
+function SingleViewer() {
   // Layout and styles.
   const Wrapper = styled(Box)`
     width: 100%;
@@ -75,20 +60,18 @@ const SingleViewer = React.memo(function SingleViewer(props: {
     searchParamKey
   );
   const initialServer =
-    props.panelKey < servers.length
-      ? servers[props.panelKey]
-      : getDefaultServerFromUrl();
+    servers.length >= 1 ? servers[0] : getDefaultServerFromUrl();
 
   // Values that can be globally accessed by components in a viewer.
   const viewer: ViewerContextContents = {
-    panelKey: props.panelKey,
     useSceneTree: useSceneTreeState(),
     useGui: useGuiState(initialServer),
     websocketRef: React.useRef(null),
     wrapperRef: React.useRef(null),
-    globalCameras: props.globalCameras,
     objFromSceneNodeNameRef: React.useRef({}),
     sceneRef: React.useRef(null),
+    cameraRef: React.useRef(null),
+    cameraControlRef: React.useRef(null),
   };
   return (
     <ViewerContext.Provider value={viewer}>
@@ -99,10 +82,10 @@ const SingleViewer = React.memo(function SingleViewer(props: {
       </Wrapper>
     </ViewerContext.Provider>
   );
-});
+}
 
 function ViewerCanvas() {
-  const viewer = useContext(ViewerContext)!;
+  const viewer = React.useContext(ViewerContext)!;
   const canvas_background_color = viewer.useGui(
     (state) => state.theme.canvas_background_color
   );
@@ -136,16 +119,6 @@ function SceneContextSetter() {
 }
 
 function Root() {
-  const globalCameras = useRef<CameraPrimitives>({
-    synchronize: false,
-    cameras: [],
-    cameraControlRefs: [],
-  });
-  const [panelCount, setPanelCount] = useState(
-    Math.max(1, getServersFromSearchParams().length)
-  );
-  const isPortrait = useMediaQuery("(orientation: portrait)");
-
   return (
     <Box
       component="div"
@@ -157,92 +130,17 @@ function Root() {
         paddingBottom: "2.5em",
       }}
     >
-      <PanelController
-        panelCount={panelCount}
-        setPanelCount={setPanelCount}
-        globalCameras={globalCameras}
-      />
-      {Array.from({ length: panelCount }, (_, i) => {
-        return (
-          <Box
-            component="div"
-            key={"box-" + i.toString()}
-            sx={{
-              ...(isPortrait
-                ? {
-                    width: "100%",
-                    height: (100.0 / panelCount).toString() + "%",
-                  }
-                : {
-                    height: "100%",
-                    float: "left",
-                    width: (100.0 / panelCount).toString() + "%",
-                  }),
-              boxSizing: "border-box",
-              position: "relative",
-              "&:not(:last-child)": {
-                borderRight: isPortrait ? null : "1px solid",
-                borderBottom: isPortrait ? "1px solid" : null,
-                borderColor: "divider",
-              },
-            }}
-          >
-            <SingleViewer panelKey={i} globalCameras={globalCameras} />
-          </Box>
-        );
-      })}
-    </Box>
-  );
-}
-
-function PanelController(props: {
-  panelCount: number;
-  setPanelCount: React.Dispatch<React.SetStateAction<number>>;
-  globalCameras: MutableRefObject<CameraPrimitives>;
-}) {
-  return (
-    <Box
-      component="div"
-      sx={{
-        position: "fixed",
-        bottom: "0",
-        width: "100%",
-        height: "2.5em",
-        zIndex: "1000",
-        backgroundColor: "rgba(255, 255, 255, 0.85)",
-        borderTop: "1px solid",
-        borderTopColor: "divider",
-      }}
-    >
-      <Grid sx={{ float: "right" }}>
-        <IconButton
-          onClick={() => {
-            props.setPanelCount(props.panelCount + 1);
-          }}
-        >
-          <AddCircleRounded />
-        </IconButton>
-        <IconButton
-          disabled={props.panelCount === 1}
-          onClick={() => {
-            if (props.panelCount === 1) return;
-            truncateSearchParamServers(props.panelCount - 1);
-            props.setPanelCount(props.panelCount - 1);
-          }}
-        >
-          <RemoveCircleRounded />
-        </IconButton>
-        <FormControlLabel
-          control={<Switch />}
-          label="Sync Cameras"
-          defaultChecked={props.globalCameras.current.synchronize}
-          onChange={(_event, checked) => {
-            props.globalCameras.current.synchronize = checked;
-          }}
-          sx={{ pl: 1 }}
-          disabled={props.panelCount === 1}
-        />
-      </Grid>
+      <Box
+        component="div"
+        sx={{
+          width: "100%",
+          height: "100%",
+          boxSizing: "border-box",
+          position: "relative",
+        }}
+      >
+        <SingleViewer />
+      </Box>
     </Box>
   );
 }
