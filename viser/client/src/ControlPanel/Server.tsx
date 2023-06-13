39bf86647c927ce498fdaab595d42710fdea5f52
Brent Yi <yibrenth@gmail.com>
2023-06-13 00:49:55 -0700

    Start MUI -> mantine

--- viser/client/src/ControlPanel/Server.tsx
+++ viser/client/src/ControlPanel/Server.tsx
@@ -1,88 +1,96 @@
-import { button, LevaPanel, useControls, useCreateStore } from "leva";
-import Box from "@mui/material/Box";
-import { levaTheme } from "./Generated";
-import { useContext, useState } from "react";
+import React from "react";
 import { ViewerContext } from "..";
 import { isTexture } from "../WebsocketInterface";
 import { Stats } from "@react-three/drei";
+import { Modal, TextInput, Button, Stack, Switch } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import SceneTreeTable from "./SceneTreeTable";
+import { IconBinaryTree, IconPhoto } from "@tabler/icons-react";
 
 export default function ServerControls() {
-  const { useGui, wrapperRef, sceneRef } = useContext(ViewerContext)!;
+  const viewer = React.useContext(ViewerContext)!;
+  const [sceneTreeOpened, { open: openSceneTree, close: closeSceneTree }] =
+    useDisclosure(false);
+  const [showStats, setShowStats] = React.useState(false);
 
-  const [showStats, setShowStats] = useState(false);
+  function triggerBlur(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key != "Enter") return;
+    event.currentTarget.blur();
+    event.currentTarget.focus();
+  }
 
-  const server = useGui((state) => state.server);
-  const label = useGui((state) => state.label);
-  const backgroundAvailable = useGui((state) => state.backgroundAvailable);
-
-  // Hack around leva bug: https://github.com/pmndrs/leva/issues/253
-  // (in case the user also makes an input called Statistics!)
-  const idPrefix = "_viser-server-";
-  const levaStore = useCreateStore();
-  useControls(
-    {
-      Label: {
-        value: label,
-        onChange: (value) => useGui.setState({ label: value }),
-      },
-      Websocket: {
-        value: server,
-        onChange: (value) => useGui.setState({ server: value }),
-      },
-      "Download Background": button(
-        () => {
-          const wrapper = wrapperRef.current;
-          if (wrapper === null) return;
-
-          if (!isTexture(sceneRef!.current!.background)) {
-            // This should never happen.
-            alert("No background to download!");
-            return;
-          }
-
-          const data = sceneRef!.current!.background.image.src;
-          console.log(data);
-          const link = document.createElement("a");
-          link.download = "background";
-          link.href = data;
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-        },
-        { disabled: !backgroundAvailable }
-      ),
-      // Note: statistics are currently global, not per-pane.
-      [idPrefix + "Statistics"]: {
-        label: "Statistics",
-        value: showStats,
-        onChange: (value) => setShowStats(value),
-      },
-    },
-    { store: levaStore },
-    [backgroundAvailable]
-  );
-
-  // Leva theming is a bit limited, so we hack at styles here...
   return (
-    <Box
-      component="div"
-      sx={{
-        "& label": { color: "#777" },
-        "& input[type='checkbox']~label svg path": {
-          stroke: "#fff !important",
-        },
-        "& button": { color: "#fff !important", height: "2em" },
-      }}
-    >
-      <LevaPanel
-        fill
-        flat
-        titleBar={false}
-        theme={levaTheme}
-        store={levaStore}
-        hideCopyButton
-      />
-      {showStats ? <Stats parent={wrapperRef} className="stats-panel" /> : null}
-    </Box>
+    <>
+      <Modal
+        padding="0"
+        withCloseButton={false}
+        opened={sceneTreeOpened}
+        onClose={closeSceneTree}
+        size="xl"
+        centered
+      >
+        <SceneTreeTable />
+      </Modal>
+      {showStats ? (
+        <Stats parent={viewer.wrapperRef} className="stats-panel" />
+      ) : null}
+      <Stack spacing="xs">
+        <TextInput
+          label="Label"
+          defaultValue={viewer.useGui((state) => state.label)}
+          onBlur={(event) =>
+            viewer.useGui.setState({ label: event.currentTarget.value })
+          }
+          onKeyDown={triggerBlur}
+        />
+        <TextInput
+          label="Server"
+          defaultValue={viewer.useGui((state) => state.server)}
+          onBlur={(event) =>
+            viewer.useGui.setState({ server: event.currentTarget.value })
+          }
+          onKeyDown={triggerBlur}
+        />
+        <Button
+          onClick={() => {
+            const wrapper = viewer.wrapperRef.current;
+            if (wrapper === null) return;
+
+            if (!isTexture(viewer.sceneRef.current!.background)) {
+              // This should never happen.
+              alert("No background to download!");
+              return;
+            }
+
+            const data = viewer.sceneRef.current!.background.image.src;
+            console.log(data);
+            const link = document.createElement("a");
+            link.download = "background";
+            link.href = data;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }}
+          fullWidth
+          disabled={!viewer.useGui((state) => state.backgroundAvailable)}
+          leftIcon={<IconPhoto size="1rem" />}
+        >
+          Download Background
+        </Button>
+        <Button
+          onClick={openSceneTree}
+          fullWidth
+          leftIcon={<IconBinaryTree size="1rem" />}
+        >
+          Scene Tree
+        </Button>
+        <Switch
+          label="WebGL Statistics"
+          onChange={(event) => {
+            setShowStats(event.currentTarget.checked);
+          }}
+        />
+      </Stack>
+    </>
   );
 }
