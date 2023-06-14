6ba95dd7e600d548ec2514d829429e8effe229d6
Brent Yi <yibrenth@gmail.com>
2023-06-14 01:14:48 -0700

    Add back scene tree tab

--- viser/client/.eslintrc.js
+++ viser/client/.eslintrc.js
@@ -31,5 +31,11 @@ module.exports = {
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "react/prop-types": [
+      "error",
+      {
+        skipUndeclared: true,
+      },
+    ],
   },
 };
