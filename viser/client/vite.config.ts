e3ebc9b60b4a322714fcd852a7f92bb781a9f7ae
Jonah Bedouch <jonahbedouch@gmail.com>
2023-06-30 22:01:55 -0700

    Switch away from Create-React-App towards Vite (#62)
    * Switch over to Vite instead of Create-React-App
    
    * Install ESLint extension to show errors on dev build
    
    * Update default host port to 3000
    
    * Remove dependencies related to Jest unit testing
    
    They are not currently utilized, and Vitest is Vite's recommended Unit Test library if there is interest in implementing them

---
+++ viser/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'build',
+    target: browserslistToEsbuild()
+  },
+});
