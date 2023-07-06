35feb00e873530ac4d20cf871bee4f2779d87e5c
Jonah Bedouch <jonahbedouch@gmail.com>
2023-07-06 05:42:03 -0700

    Viser mobile view (#60)
    * Update Viser host IP to 0.0.0.0
    
    Allows devices on a local network to connect to a websocket instance
    
    * Update Titlebar to be responsive
    
    * Create Bottom Bar component that replaces floating window on small displays
    
    * Update Floating Panel to be touch draggable
    
    * Resolve DragEvents interface typo
    
    Also fix bug related to dragging floating panel with finger due to the way that touch events send.
    
    * Increase the size of the bottom sheet handle
    
    * Flip carets, utilize type guards, and do some code cleanup
    
    * Use a MediaQuery component to prevent a re-render of the WebsocketInterface or ViewerCanvas on resize
    
    * Trim covered part of canvas when bottom bar is displayed
    
    * Run prettier
    
    ---------
    
    Co-authored-by: Brent Yi <yibrenth@gmail.com>

--- viser/client/src/index.tsx
+++ viser/client/src/index.tsx
@@ -14,10 +14,16 @@ import {
 import { BlendFunction, KernelSize } from "postprocessing";
 
 import { SynchronizedCameraControls } from "./CameraControls";
-import { Box, MantineProvider, ScrollArea } from "@mantine/core";
+import {
+  Aside,
+  Box,
+  MantineProvider,
+  MediaQuery,
+  ScrollArea,
+  useMantineTheme,
+} from "@mantine/core";
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import {
   SceneNodeThreeObject,
   UseSceneTree,
@@ -33,6 +39,8 @@ import WebsocketInterface from "./WebsocketInterface";
 
 import FloatingPanel from "./ControlPanel/FloatingPanel";
 import { Titlebar } from "./Titlebar";
+import { useMediaQuery } from "@mantine/hooks";
+import BottomPanel from "./ControlPanel/BottomPanel";
 
 type ViewerContextContents = {
   useSceneTree: UseSceneTree;
@@ -44,12 +52,12 @@ type ViewerContextContents = {
   cameraControlRef: React.MutableRefObject<CameraControls | null>;
   nodeAttributesFromName: React.MutableRefObject<{
     [name: string]:
-    | undefined
-    | {
-      wxyz?: [number, number, number, number];
-      position?: [number, number, number];
-      visibility?: boolean;
-    };
+      | undefined
+      | {
+          wxyz?: [number, number, number, number];
+          position?: [number, number, number];
+          visibility?: boolean;
+        };
   }>;
 };
 export const ViewerContext = React.createContext<null | ViewerContextContents>(
@@ -100,65 +108,89 @@ function SingleViewer() {
         }}
       >
         <WebsocketInterface />
-        <Box
-          sx={(theme) => ({
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: fixed_sidebar ? "20em" : 0,
-            position: "absolute",
-            backgroundColor:
-              theme.colorScheme === "light" ? "#fff" : theme.colors.dark[9],
-          })}
-        >
-          <ViewerCanvas />
-        </Box>
-        {fixed_sidebar ? (
+        <MediaQuery smallerThan={"sm"} styles={{ right: 0, bottom: "3.5em" }}>
           <Box
             sx={(theme) => ({
-              width: "20em",
-              boxSizing: "border-box",
-              right: 0,
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: fixed_sidebar ? "20em" : 0,
               position: "absolute",
-              top: "0em",
-              bottom: "0em",
-              borderLeft: "1px solid",
-              borderColor:
-                theme.colorScheme === "light"
-                  ? theme.colors.gray[4]
-                  : theme.colors.dark[4],
+              backgroundColor:
+                theme.colorScheme === "light" ? "#fff" : theme.colors.dark[9],
             })}
           >
-            <ScrollArea type="always" sx={{ height: "100%" }}>
-              <Box
-                p="sm"
-                sx={(theme) => ({
-                  backgroundColor:
-                    theme.colorScheme === "dark"
-                      ? theme.colors.dark[5]
-                      : theme.colors.gray[1],
-                  lineHeight: "1.5em",
-                  fontWeight: 400,
-                })}
-              >
-                <ConnectionStatus />
-              </Box>
-              <ControlPanel />
-            </ScrollArea>
+            <ViewerCanvas />
           </Box>
-        ) : (
-          <FloatingPanel>
-            <FloatingPanel.Handle>
-              <ConnectionStatus />
-            </FloatingPanel.Handle>
-            <FloatingPanel.Contents>
-              <ControlPanel />
-            </FloatingPanel.Contents>
-          </FloatingPanel>
-        )}
+        </MediaQuery>
+        <Panel fixed_sidebar={fixed_sidebar} />
       </Box>
     </ViewerContext.Provider>
   );
+}
+
+function Panel(props: { fixed_sidebar: boolean }) {
+  const theme = useMantineTheme();
+  const smQuery = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  if (smQuery) {
+    return (
+      <BottomPanel>
+        <BottomPanel.Handle>
+          <ConnectionStatus />
+        </BottomPanel.Handle>
+        <BottomPanel.Contents>
+          <ControlPanel />
+        </BottomPanel.Contents>
+      </BottomPanel>
+    );
+  } else if (props.fixed_sidebar) {
+    return (
+      <Aside
+        hiddenBreakpoint={"sm"}
+        sx={(theme) => ({
+          width: "20em",
+          boxSizing: "border-box",
+          right: 0,
+          position: "absolute",
+          top: "0em",
+          bottom: "0em",
+          borderLeft: "1px solid",
+          borderColor:
+            theme.colorScheme == "light"
+              ? theme.colors.gray[4]
+              : theme.colors.dark[4],
+        })}
+      >
+        <ScrollArea type="always" sx={{ height: "100%" }}>
+          <Box
+            p="sm"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme == "dark"
+                  ? theme.colors.dark[5]
+                  : theme.colors.gray[1],
+              lineHeight: "1.5em",
+              fontWeight: 400,
+            })}
+          >
+            <ConnectionStatus />
+          </Box>
+          <ControlPanel />
+        </ScrollArea>
+      </Aside>
+    );
+  } else {
+    return (
+      <FloatingPanel>
+        <FloatingPanel.Handle>
+          <ConnectionStatus />
+        </FloatingPanel.Handle>
+        <FloatingPanel.Contents>
+          <ControlPanel />
+        </FloatingPanel.Contents>
+      </FloatingPanel>
+    );
+  }
 }
 
 function ViewerCanvas() {
