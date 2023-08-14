260fb801f6f0fa3cc7085a10cd09ddc76e7570fe
Justin Kerr <justin.g.kerr@gmail.com>
2023-08-14 10:36:22 -0700

    make popup example

--- examples/17_popup_image.py
+++ examples/17_popup_image.py
@@ -16,19 +16,18 @@ from typing import Optional
 import numpy as onp
 
 import viser
-import viser.transforms as tf
 import trimesh 
 
 server = viser.ViserServer()
-num_frames = 20
 
 
 @server.on_client_connect
 def _(client: viser.ClientHandle) -> None:
-    img = onp.random.randint(0,255, size=(100, 100, 3), dtype=onp.uint8)
-    depth = onp.eye(100, dtype=onp.float32)[...,None]+.5
-    # depth = onp.random.rand(100,100,1).astype(onp.float32)
-    print(depth.shape)
+    img = onp.random.randint(0,255, size=(1000, 1000, 3), dtype=onp.uint8)
+    depth = onp.ones((1000,1000,1), dtype=onp.float32)
+    #make a middle square portal
+    depth[250:750,250:750,:] = 10.0
+    img[250:750,250:750,:] = 255
     mesh = trimesh.creation.box((0.5, 0.5, 0.5))
     handle = server.add_mesh_trimesh(
                 name=f"/cube",
