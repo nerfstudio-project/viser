a729c25ea7f20efd9e8f0bf7f4f0dae70068d164
Brent Yi <yibrenth@gmail.com>
2023-10-26 15:54:25 -0700

    Add configurable control panel width

--- src/viser/client/src/ControlPanel/SidebarPanel.tsx
+++ src/viser/client/src/ControlPanel/SidebarPanel.tsx
@@ -14,9 +14,11 @@ export const SidebarPanelContext = React.createContext<null | {
 export default function SidebarPanel({
   children,
   collapsible,
+  width,
 }: {
   children: string | React.ReactNode;
   collapsible: boolean;
+  width: string;
 }) {
   const [collapsed, { toggle: toggleCollapsed }] = useDisclosure(false);
 
@@ -65,7 +67,7 @@ export default function SidebarPanel({
         shadow="xl"
         component={ScrollArea}
         sx={{
-          width: collapsed ? 0 : "20em",
+          width: collapsed ? 0 : width,
           boxSizing: "content-box",
           transition: "width 0.5s 0s",
           zIndex: 10,
@@ -74,7 +76,7 @@ export default function SidebarPanel({
         <Box
           /* Prevent DOM reflow, as well as internals from getting too wide.
            * Hardcoded to match the width of the wrapper element above. */
-          w="20em"
+          w={width}
         >
           {children}
         </Box>
