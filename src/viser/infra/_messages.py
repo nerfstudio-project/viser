45898ec999a316f715b99c4023fd1d184cb85730
Brent Yi <yibrenth@gmail.com>
2023-12-14 15:42:36 +0000

    Run black

--- src/viser/infra/_messages.py
+++ src/viser/infra/_messages.py
@@ -48,7 +48,9 @@ def _prepare_for_serialization(value: Any, annotation: Type) -> Any:
                 out.append(
                     # Hack to be OK with wrong type annotations.
                     # https://github.com/nerfstudio-project/nerfstudio/pull/1805
-                    _prepare_for_serialization(v, args[i]) if i < len(args) else v
+                    _prepare_for_serialization(v, args[i])
+                    if i < len(args)
+                    else v
                 )
             return tuple(out)
 
