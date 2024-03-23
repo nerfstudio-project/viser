f4bac0ff6a818d62d3fe9889fed3c0e57e63bed0
Brent Yi <yibrenth@gmail.com>
2024-03-23 01:49:52 -0700

    Fix for POSIX systems

--- src/viser/_client_autobuild.py
+++ src/viser/_client_autobuild.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -57,13 +58,16 @@ def ensure_client_is_built() -> None:
 
     # Install nodejs and build if necessary. We assume bash is installed.
     if build:
-        node_exec_dir = _install_sandboxed_node()
-        npx_path = node_exec_dir / "npx"
+        node_bin_dir = _install_sandboxed_node()
+        npx_path = node_bin_dir / "npx"
 
-        import os
         subprocess_env = os.environ.copy()
-        subprocess_env["NODE_VIRTUAL_ENV"] = str(node_exec_dir.parent)
-        subprocess_env["PATH"] = str(node_exec_dir) + ";" + subprocess_env["PATH"] 
+        subprocess_env["NODE_VIRTUAL_ENV"] = str(node_bin_dir.parent)
+        subprocess_env["PATH"] = (
+            str(node_bin_dir)
+            + (";" if sys.platform == "win32" else ":")
+            + subprocess_env["PATH"]
+        )
         subprocess.run(
             args=f"{npx_path} --yes yarn install",
             env=subprocess_env,
@@ -82,23 +86,31 @@ def ensure_client_is_built() -> None:
 
 def _install_sandboxed_node() -> Path:
     """Install a sandboxed copy of nodejs using nodeenv, and return a path to the
-    environment root."""
-    env_dir = client_dir / ".nodeenv"
-    node_exec_dir = env_dir / "bin"
+    environment's bin directory (`.nodeenv/bin` or `.nodeenv/Scripts`).
 
-    """On windows inside .nodeenv no bin-folder exists. Instead the executables are kept in Scripts."""
-    if not node_exec_dir.exists():
-        node_exec_dir = env_dir / "Scripts"
+    On Windows, the `.nodeenv/bin` does not exist. Instead, executables are
+    installed to `.nodeenv/Scripts`."""
 
-    if (node_exec_dir / "npx").exists():
+    def get_node_bin_dir() -> Path:
+        env_dir = client_dir / ".nodeenv"
+        node_bin_dir = env_dir / "bin"
+        if not node_bin_dir.exists():
+            node_bin_dir = env_dir / "Scripts"
+        return node_bin_dir
+
+    node_bin_dir = get_node_bin_dir()
+    if (node_bin_dir / "npx").exists():
         rich.print("[bold](viser)[/bold] nodejs is set up!")
-        return node_exec_dir
+        return node_bin_dir
 
+    env_dir = client_dir / ".nodeenv"
     subprocess.run(
         [sys.executable, "-m", "nodeenv", "--node=20.4.0", env_dir], check=False
     )
-    assert (node_exec_dir / "npx").exists()
-    return node_exec_dir
+
+    node_bin_dir = get_node_bin_dir()
+    assert (node_bin_dir / "npx").exists()
+    return node_bin_dir
 
 
 def _modified_time_recursive(dir: Path) -> float:
