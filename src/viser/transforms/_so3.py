d01a108439e384184ae92604d7928f70ea425568
Brent Yi <yibrenth@gmail.com>
2024-05-05 23:34:50 -0700

    Support batch axes + broadcasting for `viser.transforms`

--- src/viser/transforms/_so3.py
+++ src/viser/transforms/_so3.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import dataclasses
+from typing import Tuple
 
 import numpy as onp
 import numpy.typing as onpt
 from typing_extensions import override
 
 from . import _base, hints
-from .utils import get_epsilon, register_lie_group
+from .utils import broadcast_leading_axes, get_epsilon, register_lie_group
 
 
 @register_lie_group(
@@ -16,20 +17,17 @@ from .utils import get_epsilon, register_lie_group
     tangent_dim=3,
     space_dim=3,
 )
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class SO3(_base.SOBase):
-    """Special orthogonal group for 3D rotations.
-
-    Ported to numpy from `jaxlie.SO3`.
+    """Special orthogonal group for 3D rotations. Broadcasting rules are the same as
+    for numpy.
 
     Internal parameterization is `(qw, qx, qy, qz)`. Tangent parameterization is
     `(omega_x, omega_y, omega_z)`.
     """
 
-    # SO3-specific.
-
-    wxyz: onpt.NDArray[onp.floating]
-    """Internal parameters. `(w, x, y, z)` quaternion."""
+    wxyz: onp.ndarray
+    """Internal parameters. `(w, x, y, z)` quaternion. Shape should be `(*, 4)`."""
 
     @override
     def __repr__(self) -> str:
@@ -46,7 +44,8 @@ class SO3(_base.SOBase):
         Returns:
             Output.
         """
-        return SO3.exp(onp.array([theta, 0.0, 0.0]))
+        zeros = onp.zeros_like(theta)
+        return SO3.exp(onp.stack([theta, zeros, zeros], axis=-1))
 
     @staticmethod
     def from_y_radians(theta: hints.Scalar) -> SO3:
@@ -58,7 +57,8 @@ class SO3(_base.SOBase):
         Returns:
             Output.
         """
-        return SO3.exp(onp.array([0.0, theta, 0.0]))
+        zeros = onp.zeros_like(theta)
+        return SO3.exp(onp.stack([zeros, theta, zeros], axis=-1))
 
     @staticmethod
     def from_z_radians(theta: hints.Scalar) -> SO3:
@@ -70,7 +70,8 @@ class SO3(_base.SOBase):
         Returns:
             Output.
         """
-        return SO3.exp(onp.array([0.0, 0.0, theta]))
+        zeros = onp.zeros_like(theta)
+        return SO3.exp(onp.stack([zeros, zeros, theta], axis=-1))
 
     @staticmethod
     def from_rpy_radians(
@@ -96,24 +97,24 @@ class SO3(_base.SOBase):
         )
 
     @staticmethod
-    def from_quaternion_xyzw(xyzw: onpt.NDArray[onp.floating]) -> SO3:
+    def from_quaternion_xyzw(xyzw: hints.Array) -> SO3:
         """Construct a rotation from an `xyzw` quaternion.
 
         Note that `wxyz` quaternions can be constructed using the default dataclass
         constructor.
 
         Args:
-            xyzw: xyzw quaternion. Shape should be (4,).
+            xyzw: xyzw quaternion. Shape should be (*, 4).
 
         Returns:
             Output.
         """
-        assert xyzw.shape == (4,)
-        return SO3(onp.roll(xyzw, shift=1))
+        assert xyzw.shape[-1:] == (4,)
+        return SO3(onp.roll(xyzw, axis=-1, shift=1))
 
     def as_quaternion_xyzw(self) -> onpt.NDArray[onp.floating]:
         """Grab parameters as xyzw quaternion."""
-        return onp.roll(self.wxyz, shift=-1)
+        return onp.roll(self.wxyz, axis=-1, shift=-1)
 
     def as_rpy_radians(self) -> hints.RollPitchYaw:
         """Computes roll, pitch, and yaw angles. Uses the ZYX mobile robot convention.
@@ -134,7 +135,7 @@ class SO3(_base.SOBase):
             Euler angle in radians.
         """
         # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_conversion
-        q0, q1, q2, q3 = self.wxyz
+        q0, q1, q2, q3 = onp.moveaxis(self.wxyz, -1, 0)
         return onp.arctan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (q1**2 + q2**2))
 
     def compute_pitch_radians(self) -> onpt.NDArray[onp.floating]:
@@ -144,7 +145,7 @@ class SO3(_base.SOBase):
             Euler angle in radians.
         """
         # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_conversion
-        q0, q1, q2, q3 = self.wxyz
+        q0, q1, q2, q3 = onp.moveaxis(self.wxyz, -1, 0)
         return onp.arcsin(2 * (q0 * q2 - q3 * q1))
 
     def compute_yaw_radians(self) -> onpt.NDArray[onp.floating]:
@@ -154,70 +155,76 @@ class SO3(_base.SOBase):
             Euler angle in radians.
         """
         # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_angles_conversion
-        q0, q1, q2, q3 = self.wxyz
+        q0, q1, q2, q3 = onp.moveaxis(self.wxyz, -1, 0)
         return onp.arctan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2**2 + q3**2))
 
     # Factory.
 
-    @override
     @classmethod
-    def identity(cls) -> SO3:
-        return SO3(wxyz=onp.array([1.0, 0.0, 0.0, 0.0]))
+    @override
+    def identity(cls, batch_axes: Tuple[int, ...] = ()) -> SO3:
+        return SO3(
+            wxyz=onp.broadcast_to(onp.array([1.0, 0.0, 0.0, 0.0]), (*batch_axes, 4))
+        )
 
-    @override
     @classmethod
-    def from_matrix(cls, matrix: onpt.NDArray[onp.floating]) -> SO3:
-        assert matrix.shape == (3, 3)
+    @override
+    def from_matrix(cls, matrix: hints.Array) -> SO3:
+        assert matrix.shape[-2:] == (3, 3)
 
         # Modified from:
         # > "Converting a Rotation Matrix to a Quaternion" from Mike Day
         # > https://d3cw3dd2w32x2b.cloudfront.net/wp-content/uploads/2015/01/matrix-to-quat.pdf
 
         def case0(m):
-            t = 1 + m[0, 0] - m[1, 1] - m[2, 2]
-            q = onp.array(
+            t = 1 + m[..., 0, 0] - m[..., 1, 1] - m[..., 2, 2]
+            q = onp.stack(
                 [
-                    m[2, 1] - m[1, 2],
+                    m[..., 2, 1] - m[..., 1, 2],
                     t,
-                    m[1, 0] + m[0, 1],
-                    m[0, 2] + m[2, 0],
-                ]
+                    m[..., 1, 0] + m[..., 0, 1],
+                    m[..., 0, 2] + m[..., 2, 0],
+                ],
+                axis=-1,
             )
             return t, q
 
         def case1(m):
-            t = 1 - m[0, 0] + m[1, 1] - m[2, 2]
-            q = onp.array(
+            t = 1 - m[..., 0, 0] + m[..., 1, 1] - m[..., 2, 2]
+            q = onp.stack(
                 [
-                    m[0, 2] - m[2, 0],
-                    m[1, 0] + m[0, 1],
+                    m[..., 0, 2] - m[..., 2, 0],
+                    m[..., 1, 0] + m[..., 0, 1],
                     t,
-                    m[2, 1] + m[1, 2],
-                ]
+                    m[..., 2, 1] + m[..., 1, 2],
+                ],
+                axis=-1,
             )
             return t, q
 
         def case2(m):
-            t = 1 - m[0, 0] - m[1, 1] + m[2, 2]
-            q = onp.array(
+            t = 1 - m[..., 0, 0] - m[..., 1, 1] + m[..., 2, 2]
+            q = onp.stack(
                 [
-                    m[1, 0] - m[0, 1],
-                    m[0, 2] + m[2, 0],
-                    m[2, 1] + m[1, 2],
+                    m[..., 1, 0] - m[..., 0, 1],
+                    m[..., 0, 2] + m[..., 2, 0],
+                    m[..., 2, 1] + m[..., 1, 2],
                     t,
-                ]
+                ],
+                axis=-1,
             )
             return t, q
 
         def case3(m):
-            t = 1 + m[0, 0] + m[1, 1] + m[2, 2]
-            q = onp.array(
+            t = 1 + m[..., 0, 0] + m[..., 1, 1] + m[..., 2, 2]
+            q = onp.stack(
                 [
                     t,
-                    m[2, 1] - m[1, 2],
-                    m[0, 2] - m[2, 0],
-                    m[1, 0] - m[0, 1],
-                ]
+                    m[..., 2, 1] - m[..., 1, 2],
+                    m[..., 0, 2] - m[..., 2, 0],
+                    m[..., 1, 0] - m[..., 0, 1],
+                ],
+                axis=-1,
             )
             return t, q
 
@@ -228,9 +235,9 @@ class SO3(_base.SOBase):
         case2_t, case2_q = case2(matrix)
         case3_t, case3_q = case3(matrix)
 
-        cond0 = matrix[2, 2] < 0
-        cond1 = matrix[0, 0] > matrix[1, 1]
-        cond2 = matrix[0, 0] < -matrix[1, 1]
+        cond0 = matrix[..., 2, 2] < 0
+        cond1 = matrix[..., 0, 0] > matrix[..., 1, 1]
+        cond2 = matrix[..., 0, 0] < -matrix[..., 1, 1]
 
         t = onp.where(
             cond0,
@@ -238,9 +245,9 @@ class SO3(_base.SOBase):
             onp.where(cond2, case2_t, case3_t),
         )
         q = onp.where(
-            cond0,
-            onp.where(cond1, case0_q, case1_q),
-            onp.where(cond2, case2_q, case3_q),
+            cond0[..., None],
+            onp.where(cond1[..., None], case0_q, case1_q),
+            onp.where(cond2[..., None], case2_q, case3_q),
         )
 
         # We can also choose to branch, but this is slower.
@@ -261,22 +268,29 @@ class SO3(_base.SOBase):
         #     operand=matrix,
         # )
 
-        return SO3(wxyz=q * 0.5 / onp.sqrt(t))
+        return SO3(wxyz=q * 0.5 / onp.sqrt(t[..., None]))
 
     # Accessors.
 
     @override
     def as_matrix(self) -> onpt.NDArray[onp.floating]:
-        norm = self.wxyz @ self.wxyz
-        q = self.wxyz * onp.sqrt(2.0 / norm)
-        q = onp.outer(q, q)
-        return onp.array(
+        norm_sq = onp.sum(onp.square(self.wxyz), axis=-1, keepdims=True)
+        q = self.wxyz * onp.sqrt(2.0 / norm_sq)  # (*, 4)
+        q_outer = onp.einsum("...i,...j->...ij", q, q)  # (*, 4, 4)
+        return onp.stack(
             [
-                [1.0 - q[2, 2] - q[3, 3], q[1, 2] - q[3, 0], q[1, 3] + q[2, 0]],
-                [q[1, 2] + q[3, 0], 1.0 - q[1, 1] - q[3, 3], q[2, 3] - q[1, 0]],
-                [q[1, 3] - q[2, 0], q[2, 3] + q[1, 0], 1.0 - q[1, 1] - q[2, 2]],
-            ]
-        )
+                1.0 - q_outer[..., 2, 2] - q_outer[..., 3, 3],
+                q_outer[..., 1, 2] - q_outer[..., 3, 0],
+                q_outer[..., 1, 3] + q_outer[..., 2, 0],
+                q_outer[..., 1, 2] + q_outer[..., 3, 0],
+                1.0 - q_outer[..., 1, 1] - q_outer[..., 3, 3],
+                q_outer[..., 2, 3] - q_outer[..., 1, 0],
+                q_outer[..., 1, 3] - q_outer[..., 2, 0],
+                q_outer[..., 2, 3] + q_outer[..., 1, 0],
+                1.0 - q_outer[..., 1, 1] - q_outer[..., 2, 2],
+            ],
+            axis=-1,
+        ).reshape(*q.shape[:-1], 3, 3)
 
     @override
     def parameters(self) -> onpt.NDArray[onp.floating]:
@@ -285,45 +299,50 @@ class SO3(_base.SOBase):
     # Operations.
 
     @override
-    def apply(self, target: onpt.NDArray[onp.floating]) -> onpt.NDArray[onp.floating]:
-        assert target.shape == (3,)
+    def apply(self, target: hints.Array) -> onpt.NDArray[onp.floating]:
+        assert target.shape[-1:] == (3,)
+        self, target = broadcast_leading_axes((self, target))
 
         # Compute using quaternion multiplys.
-        padded_target = onp.concatenate([onp.zeros(1), target])
-        return (self @ SO3(wxyz=padded_target) @ self.inverse()).wxyz[1:]
+        padded_target = onp.concatenate(
+            [onp.zeros((*self.get_batch_axes(), 1)), target], axis=-1
+        )
+        return (self @ SO3(wxyz=padded_target) @ self.inverse()).wxyz[..., 1:]
 
     @override
     def multiply(self, other: SO3) -> SO3:
-        w0, x0, y0, z0 = self.wxyz
-        w1, x1, y1, z1 = other.wxyz
+        w0, x0, y0, z0 = onp.moveaxis(self.wxyz, -1, 0)
+        w1, x1, y1, z1 = onp.moveaxis(other.wxyz, -1, 0)
         return SO3(
-            wxyz=onp.array(
+            wxyz=onp.stack(
                 [
                     -x0 * x1 - y0 * y1 - z0 * z1 + w0 * w1,
                     x0 * w1 + y0 * z1 - z0 * y1 + w0 * x1,
                     -x0 * z1 + y0 * w1 + z0 * x1 + w0 * y1,
                     x0 * y1 - y0 * x1 + z0 * w1 + w0 * z1,
-                ]
+                ],
+                axis=-1,
             )
         )
 
     @classmethod
     @override
-    def exp(cls, tangent: onpt.NDArray[onp.floating]) -> SO3:
+    def exp(cls, tangent: hints.Array) -> SO3:
         # Reference:
         # > https://github.com/strasdat/Sophus/blob/a0fe89a323e20c42d3cecb590937eb7a06b8343a/sophus/so3.hpp#L583
-        assert tangent.shape == (3,)
 
-        theta_squared = tangent @ tangent
+        assert tangent.shape[-1:] == (3,)
+
+        theta_squared = onp.sum(onp.square(tangent), axis=-1)
         theta_pow_4 = theta_squared * theta_squared
         use_taylor = theta_squared < get_epsilon(tangent.dtype)
 
         # Shim to avoid NaNs in onp.where branches, which cause failures for
-        # reverse-mode AD. (note: this is needed in JAX, but not in numpy)
+        # reverse-mode AD.
         safe_theta = onp.sqrt(
             onp.where(
                 use_taylor,
-                1.0,  # Any constant value should do here.
+                onp.ones_like(theta_squared),  # Any constant value should do here.
                 theta_squared,
             )
         )
@@ -344,9 +363,10 @@ class SO3(_base.SOBase):
         return SO3(
             wxyz=onp.concatenate(
                 [
-                    real_factor[None],
-                    imaginary_factor * tangent,
-                ]
+                    real_factor[..., None],
+                    imaginary_factor[..., None] * tangent,
+                ],
+                axis=-1,
             )
         )
 
@@ -356,11 +376,11 @@ class SO3(_base.SOBase):
         # > https://github.com/strasdat/Sophus/blob/a0fe89a323e20c42d3cecb590937eb7a06b8343a/sophus/so3.hpp#L247
 
         w = self.wxyz[..., 0]
-        norm_sq = self.wxyz[..., 1:] @ self.wxyz[..., 1:]
+        norm_sq = onp.sum(onp.square(self.wxyz[..., 1:]), axis=-1)
         use_taylor = norm_sq < get_epsilon(norm_sq.dtype)
 
         # Shim to avoid NaNs in onp.where branches, which cause failures for
-        # reverse-mode AD. (note: this is needed in JAX, but not in numpy)
+        # reverse-mode AD.
         norm_safe = onp.sqrt(
             onp.where(
                 use_taylor,
@@ -383,7 +403,7 @@ class SO3(_base.SOBase):
             ),
         )
 
-        return atan_factor * self.wxyz[1:]
+        return atan_factor[..., None] * self.wxyz[..., 1:]  # type: ignore
 
     @override
     def adjoint(self) -> onpt.NDArray[onp.floating]:
@@ -396,29 +416,36 @@ class SO3(_base.SOBase):
 
     @override
     def normalize(self) -> SO3:
-        return SO3(wxyz=self.wxyz / onp.linalg.norm(self.wxyz))
+        return SO3(wxyz=self.wxyz / onp.linalg.norm(self.wxyz, axis=-1, keepdims=True))
 
-    #  @staticmethod
-    #  @override
-    #  def sample_uniform(key: hints.KeyArray) -> SO3:
-    #      # Uniformly sample over S^3.
-    #      # > Reference: http://planning.cs.uiuc.edu/node198.html
-    #      u1, u2, u3 = jax.random.uniform(
-    #          key=key,
-    #          shape=(3,),
-    #          minval=onp.zeros(3),
-    #          maxval=onp.array([1.0, 2.0 * onp.pi, 2.0 * onp.pi]),
-    #      )
-    #      a = onp.sqrt(1.0 - u1)
-    #      b = onp.sqrt(u1)
+    # @classmethod
+    # @override
+    # def sample_uniform(
+    #     cls, key: onp.ndarray, batch_axes: jdc.Static[Tuple[int, ...]] = ()
+    # ) -> SO3:
+    #     # Uniformly sample over S^3.
+    #     # > Reference: http://planning.cs.uiuc.edu/node198.html
+    #     u1, u2, u3 = onp.moveaxis(
+    #         jax.random.uniform(
+    #             key=key,
+    #             shape=(*batch_axes, 3),
+    #             minval=onp.zeros(3),
+    #             maxval=onp.array([1.0, 2.0 * onp.pi, 2.0 * onp.pi]),
+    #         ),
+    #         -1,
+    #         0,
+    #     )
+    #     a = onp.sqrt(1.0 - u1)
+    #     b = onp.sqrt(u1)
     #
-    #      return SO3(
-    #          wxyz=onp.array(
-    #              [
-    #                  a * onp.sin(u2),
-    #                  a * onp.cos(u2),
-    #                  b * onp.sin(u3),
-    #                  b * onp.cos(u3),
-    #              ]
-    #          )
-    #      )
+    #     return SO3(
+    #         wxyz=onp.stack(
+    #             [
+    #                 a * onp.sin(u2),
+    #                 a * onp.cos(u2),
+    #                 b * onp.sin(u3),
+    #                 b * onp.cos(u3),
+    #             ],
+    #             axis=-1,
+    #         )
+    #     )
--- src/viser/transforms/utils/_utils.py
+++ src/viser/transforms/utils/_utils.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING, Any, Callable, Type, TypeVar
+from typing import TYPE_CHECKING, Callable, Tuple, Type, TypeVar, Union, cast
 
 import numpy as onp
+from jaxlie.hints import Array
 
 if TYPE_CHECKING:
     from .._base import MatrixLieGroup
@@ -9,7 +10,7 @@ if TYPE_CHECKING:
 T = TypeVar("T", bound="MatrixLieGroup")
 
 
-def get_epsilon(dtype: Any) -> float:
+def get_epsilon(dtype: onp.dtype) -> float:
     """Helper for grabbing type-specific precision constants.
 
     Args:
@@ -18,12 +19,10 @@ def get_epsilon(dtype: Any) -> float:
     Returns:
         Output float.
     """
-    if dtype == onp.float32:
-        return 1e-5
-    elif dtype == onp.float64:
-        return 1e-10
-    else:
-        assert False
+    return {
+        onp.dtype("float32"): 1e-5,
+        onp.dtype("float64"): 1e-10,
+    }[dtype]
 
 
 def register_lie_group(
@@ -35,7 +34,7 @@ def register_lie_group(
 ) -> Callable[[Type[T]], Type[T]]:
     """Decorator for registering Lie group dataclasses.
 
-    Sets dimensionality class variables, and (formerly in the JAX version) marks all methods for JIT compilation.
+    Sets dimensionality class variables, and marks all methods for JIT compilation.
     """
 
     def _wrap(cls: Type[T]) -> Type[T]:
@@ -44,7 +43,45 @@ def register_lie_group(
         cls.parameters_dim = parameters_dim
         cls.tangent_dim = tangent_dim
         cls.space_dim = space_dim
-
         return cls
 
     return _wrap
+
+
+TupleOfBroadcastable = TypeVar(
+    "TupleOfBroadcastable",
+    bound="Tuple[Union[MatrixLieGroup, Array], ...]",
+)
+
+
+def broadcast_leading_axes(inputs: TupleOfBroadcastable) -> TupleOfBroadcastable:
+    """Broadcast leading axes of arrays. Takes tuples of either:
+    - an array, which we assume has shape (*, D).
+    - a Lie group object."""
+
+    from .._base import MatrixLieGroup
+
+    array_inputs = [
+        (
+            (x.parameters(), (x.parameters_dim,))
+            if isinstance(x, MatrixLieGroup)
+            else (x, x.shape[-1:])
+        )
+        for x in inputs
+    ]
+    for array, shape_suffix in array_inputs:
+        assert array.shape[-len(shape_suffix) :] == shape_suffix
+    batch_axes = onp.broadcast_shapes(
+        *[array.shape[: -len(suffix)] for array, suffix in array_inputs]
+    )
+    broadcasted_arrays = tuple(
+        onp.broadcast_to(array, batch_axes + shape_suffix)
+        for (array, shape_suffix) in array_inputs
+    )
+    return cast(
+        TupleOfBroadcastable,
+        tuple(
+            array if not isinstance(inp, MatrixLieGroup) else type(inp)(array)
+            for array, inp in zip(broadcasted_arrays, inputs)
+        ),
+    )
