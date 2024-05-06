36ae586f02891cde772c37d0f7097f4f876df22d
Brent Yi <yibrenth@gmail.com>
2024-05-06 01:24:18 -0700

    Support batch axes + broadcasting for `viser.transforms` (#208)
    * Support batch axes + broadcasting for `viser.transforms`
    
    * Nits
    
    * Sync docs
    
    * Fix type

--- src/viser/transforms/_se3.py
+++ src/viser/transforms/_se3.py
@@ -1,28 +1,26 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import cast
+from typing import Tuple, cast
 
 import numpy as onp
 import numpy.typing as onpt
 from typing_extensions import override
 
-from . import _base
+from . import _base, hints
 from ._so3 import SO3
-from .utils import get_epsilon, register_lie_group
+from .utils import broadcast_leading_axes, get_epsilon, register_lie_group
 
 
-def _skew(omega: onpt.NDArray[onp.floating]) -> onpt.NDArray[onp.floating]:
+def _skew(omega: hints.Array) -> onpt.NDArray[onp.floating]:
     """Returns the skew-symmetric form of a length-3 vector."""
 
-    wx, wy, wz = omega
-    return onp.array(
-        [  # type: ignore
-            [0.0, -wz, wy],
-            [wz, 0.0, -wx],
-            [-wy, wx, 0.0],
-        ]
-    )
+    wx, wy, wz = onp.moveaxis(omega, -1, 0)
+    zeros = onp.zeros_like(wx)
+    return onp.stack(
+        [zeros, -wz, wy, wz, zeros, -wx, -wy, wx, zeros],
+        axis=-1,
+    ).reshape((*omega.shape[:-1], 3, 3))
 
 
 @register_lie_group(
@@ -31,11 +29,10 @@ def _skew(omega: onpt.NDArray[onp.floating]) -> onpt.NDArray[onp.floating]:
     tangent_dim=6,
     space_dim=3,
 )
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class SE3(_base.SEBase[SO3]):
-    """Special Euclidean group for proper rigid transforms in 3D.
-
-    Ported to numpy from `jaxlie.SE3`.
+    """Special Euclidean group for proper rigid transforms in 3D. Broadcasting
+    rules are the same as for numpy.
 
     Internal parameterization is `(qw, qx, qy, qz, x, y, z)`. Tangent parameterization
     is `(vx, vy, vz, omega_x, omega_y, omega_z)`.
@@ -44,7 +41,7 @@ class SE3(_base.SEBase[SO3]):
     # SE3-specific.
 
     wxyz_xyz: onpt.NDArray[onp.floating]
-    """Internal parameters. wxyz quaternion followed by xyz translation."""
+    """Internal parameters. wxyz quaternion followed by xyz translation. Shape should be `(*, 7)`."""
 
     @override
     def __repr__(self) -> str:
@@ -59,15 +56,11 @@ class SE3(_base.SEBase[SO3]):
     def from_rotation_and_translation(
         cls,
         rotation: SO3,
-        translation: onpt.NDArray[onp.floating],
+        translation: hints.Array,
     ) -> SE3:
-        assert translation.shape == (3,)
-        return SE3(wxyz_xyz=onp.concatenate([rotation.wxyz, translation]))
-
-    @override
-    @classmethod
-    def from_translation(cls, translation: onpt.NDArray[onp.floating]) -> "SE3":
-        return SE3.from_rotation_and_translation(SO3.identity(), translation)
+        assert translation.shape[-1:] == (3,)
+        rotation, translation = broadcast_leading_axes((rotation, translation))
+        return SE3(wxyz_xyz=onp.concatenate([rotation.wxyz, translation], axis=-1))
 
     @override
     def rotation(self) -> SO3:
@@ -81,34 +74,32 @@ class SE3(_base.SEBase[SO3]):
 
     @classmethod
     @override
-    def identity(cls) -> SE3:
-        return SE3(wxyz_xyz=onp.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    def identity(cls, batch_axes: Tuple[int, ...] = ()) -> SE3:
+        return SE3(
+            wxyz_xyz=onp.broadcast_to(
+                onp.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), (*batch_axes, 7)
+            )
+        )
 
     @classmethod
     @override
-    def from_matrix(cls, matrix: onpt.NDArray[onp.floating]) -> SE3:
-        assert matrix.shape == (4, 4)
+    def from_matrix(cls, matrix: hints.Array) -> SE3:
+        assert matrix.shape[-2:] == (4, 4)
         # Currently assumes bottom row is [0, 0, 0, 1].
         return SE3.from_rotation_and_translation(
-            rotation=SO3.from_matrix(matrix[:3, :3]),
-            translation=matrix[:3, 3],
+            rotation=SO3.from_matrix(matrix[..., :3, :3]),
+            translation=matrix[..., :3, 3],
         )
 
     # Accessors.
 
     @override
     def as_matrix(self) -> onpt.NDArray[onp.floating]:
-        out = onp.eye(4)
-        out[:3, :3] = self.rotation().as_matrix()
-        out[:3, 3] = self.translation()
+        out = onp.zeros((*self.get_batch_axes(), 4, 4))
+        out[..., :3, :3] = self.rotation().as_matrix()
+        out[..., :3, 3] = set(self.translation())
+        out[..., 3, 3] = 1.0
         return out
-        #  return (
-        #      onp.eye(4)
-        #      .at[:3, :3]
-        #      .set(self.rotation().as_matrix())
-        #      .at[:3, 3]
-        #      .set(self.translation())
-        #  )
 
     @override
     def parameters(self) -> onpt.NDArray[onp.floating]:
@@ -118,47 +109,50 @@ class SE3(_base.SEBase[SO3]):
 
     @classmethod
     @override
-    def exp(cls, tangent: onpt.NDArray[onp.floating]) -> SE3:
+    def exp(cls, tangent: hints.Array) -> SE3:
         # Reference:
         # > https://github.com/strasdat/Sophus/blob/a0fe89a323e20c42d3cecb590937eb7a06b8343a/sophus/se3.hpp#L761
 
         # (x, y, z, omega_x, omega_y, omega_z)
-        assert tangent.shape == (6,)
+        assert tangent.shape[-1:] == (6,)
 
-        rotation = SO3.exp(tangent[3:])
+        rotation = SO3.exp(tangent[..., 3:])
 
-        theta_squared = tangent[3:] @ tangent[3:]
+        theta_squared = onp.sum(onp.square(tangent[..., 3:]), axis=-1)
         use_taylor = theta_squared < get_epsilon(theta_squared.dtype)
 
         # Shim to avoid NaNs in onp.where branches, which cause failures for
-        # reverse-mode AD. (note: this is needed in JAX, but not in numpy)
+        # reverse-mode AD.
         theta_squared_safe = cast(
-            onpt.NDArray[onp.floating],
+            onp.ndarray,
             onp.where(
                 use_taylor,
-                1.0,  # Any non-zero value should do here.
+                onp.ones_like(theta_squared),  # Any non-zero value should do here.
                 theta_squared,
             ),
         )
         del theta_squared
         theta_safe = onp.sqrt(theta_squared_safe)
 
-        skew_omega = _skew(tangent[3:])
+        skew_omega = _skew(tangent[..., 3:])
         V = onp.where(
-            use_taylor,
+            use_taylor[..., None, None],
             rotation.as_matrix(),
             (
                 onp.eye(3)
-                + (1.0 - onp.cos(theta_safe)) / (theta_squared_safe) * skew_omega
-                + (theta_safe - onp.sin(theta_safe))
-                / (theta_squared_safe * theta_safe)
-                * (skew_omega @ skew_omega)
+                + ((1.0 - onp.cos(theta_safe)) / (theta_squared_safe))[..., None, None]
+                * skew_omega
+                + (
+                    (theta_safe - onp.sin(theta_safe))
+                    / (theta_squared_safe * theta_safe)
+                )[..., None, None]
+                * onp.einsum("...ij,...jk->...ik", skew_omega, skew_omega)
             ),
         )
 
         return SE3.from_rotation_and_translation(
             rotation=rotation,
-            translation=V @ tangent[:3],
+            translation=onp.einsum("...ij,...j->...i", V, tangent[..., :3]),
         )
 
     @override
@@ -166,16 +160,16 @@ class SE3(_base.SEBase[SO3]):
         # Reference:
         # > https://github.com/strasdat/Sophus/blob/a0fe89a323e20c42d3cecb590937eb7a06b8343a/sophus/se3.hpp#L223
         omega = self.rotation().log()
-        theta_squared = omega @ omega
+        theta_squared = onp.sum(onp.square(omega), axis=-1)
         use_taylor = theta_squared < get_epsilon(theta_squared.dtype)
 
         skew_omega = _skew(omega)
 
         # Shim to avoid NaNs in onp.where branches, which cause failures for
-        # reverse-mode AD. (note: this is needed in JAX, but not in numpy)
+        # reverse-mode AD.
         theta_squared_safe = onp.where(
             use_taylor,
-            1.0,  # Any non-zero value should do here.
+            onp.ones_like(theta_squared),  # Any non-zero value should do here.
             theta_squared,
         )
         del theta_squared
@@ -183,40 +177,54 @@ class SE3(_base.SEBase[SO3]):
         half_theta_safe = theta_safe / 2.0
 
         V_inv = onp.where(
-            use_taylor,
-            onp.eye(3) - 0.5 * skew_omega + (skew_omega @ skew_omega) / 12.0,
+            use_taylor[..., None, None],
+            onp.eye(3)
+            - 0.5 * skew_omega
+            + onp.einsum("...ij,...jk->...ik", skew_omega, skew_omega) / 12.0,
             (
                 onp.eye(3)
                 - 0.5 * skew_omega
                 + (
-                    1.0
-                    - theta_safe
-                    * onp.cos(half_theta_safe)
-                    / (2.0 * onp.sin(half_theta_safe))
-                )
-                / theta_squared_safe
-                * (skew_omega @ skew_omega)
+                    (
+                        1.0
+                        - theta_safe
+                        * onp.cos(half_theta_safe)
+                        / (2.0 * onp.sin(half_theta_safe))
+                    )
+                    / theta_squared_safe
+                )[..., None, None]
+                * onp.einsum("...ij,...jk->...ik", skew_omega, skew_omega)
             ),
         )
-        return onp.concatenate([V_inv @ self.translation(), omega])
+        return onp.concatenate(
+            [onp.einsum("...ij,...j->...i", V_inv, self.translation()), omega], axis=-1
+        )
 
     @override
     def adjoint(self) -> onpt.NDArray[onp.floating]:
         R = self.rotation().as_matrix()
-        return onp.block(
+        return onp.concatenate(
             [
-                [R, _skew(self.translation()) @ R],
-                [onp.zeros((3, 3)), R],
-            ]
+                onp.concatenate(
+                    [R, onp.einsum("...ij,...jk->...ik", _skew(self.translation()), R)],
+                    axis=-1,
+                ),
+                onp.concatenate(
+                    [onp.zeros((*self.get_batch_axes(), 3, 3)), R], axis=-1
+                ),
+            ],
+            axis=-2,
         )
 
-    # @staticmethod
+    # @classmethod
     # @override
-    # def sample_uniform(key: hints.KeyArray) -> SE3:
+    # def sample_uniform(
+    #     cls, key: onp.ndarray, batch_axes: jdc.Static[Tuple[int, ...]] = ()
+    # ) -> SE3:
     #     key0, key1 = jax.random.split(key)
     #     return SE3.from_rotation_and_translation(
-    #         rotation=SO3.sample_uniform(key0),
+    #         rotation=SO3.sample_uniform(key0, batch_axes=batch_axes),
     #         translation=jax.random.uniform(
-    #             key=key1, shape=(3,), minval=-1.0, maxval=1.0
+    #             key=key1, shape=(*batch_axes, 3), minval=-1.0, maxval=1.0
     #         ),
     #     )
--- src/viser/transforms/utils/_utils.py
+++ src/viser/transforms/utils/_utils.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Callable, Type, TypeVar
+from typing import TYPE_CHECKING, Callable, Tuple, Type, TypeVar, Union, cast
 
 import numpy as onp
 
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 T = TypeVar("T", bound="MatrixLieGroup")
 
 
-def get_epsilon(dtype: Any) -> float:
+def get_epsilon(dtype: onp.dtype) -> float:
     """Helper for grabbing type-specific precision constants.
 
     Args:
@@ -35,7 +35,7 @@ def register_lie_group(
 ) -> Callable[[Type[T]], Type[T]]:
     """Decorator for registering Lie group dataclasses.
 
-    Sets dimensionality class variables, and (formerly in the JAX version) marks all methods for JIT compilation.
+    Sets dimensionality class variables, and marks all methods for JIT compilation.
     """
 
     def _wrap(cls: Type[T]) -> Type[T]:
@@ -44,7 +44,45 @@ def register_lie_group(
         cls.parameters_dim = parameters_dim
         cls.tangent_dim = tangent_dim
         cls.space_dim = space_dim
-
         return cls
 
     return _wrap
+
+
+TupleOfBroadcastable = TypeVar(
+    "TupleOfBroadcastable",
+    bound="Tuple[Union[MatrixLieGroup, onp.ndarray], ...]",
+)
+
+
+def broadcast_leading_axes(inputs: TupleOfBroadcastable) -> TupleOfBroadcastable:
+    """Broadcast leading axes of arrays. Takes tuples of either:
+    - an array, which we assume has shape (*, D).
+    - a Lie group object."""
+
+    from .._base import MatrixLieGroup
+
+    array_inputs = [
+        (
+            (x.parameters(), (x.parameters_dim,))
+            if isinstance(x, MatrixLieGroup)
+            else (x, x.shape[-1:])
+        )
+        for x in inputs
+    ]
+    for array, shape_suffix in array_inputs:
+        assert array.shape[-len(shape_suffix) :] == shape_suffix
+    batch_axes = onp.broadcast_shapes(
+        *[array.shape[: -len(suffix)] for array, suffix in array_inputs]
+    )
+    broadcasted_arrays = tuple(
+        onp.broadcast_to(array, batch_axes + shape_suffix)
+        for (array, shape_suffix) in array_inputs
+    )
+    return cast(
+        TupleOfBroadcastable,
+        tuple(
+            array if not isinstance(inp, MatrixLieGroup) else type(inp)(array)
+            for array, inp in zip(broadcasted_arrays, inputs)
+        ),
+    )
