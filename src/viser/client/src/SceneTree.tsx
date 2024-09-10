2caeec4385ad8f230095774a1f07796198cde822
brentyi <yibrenth@gmail.com>
2024-09-10 15:34:37 -0700

    prettier

--- src/viser/client/src/SceneTree.tsx
+++ src/viser/client/src/SceneTree.tsx
@@ -222,20 +222,16 @@ function useObjectFactory(message: SceneNodeMessage | undefined): {
                 message.props.plane == "xz"
                   ? new THREE.Euler(0.0, 0.0, 0.0)
                   : message.props.plane == "xy"
-                    ? new THREE.Euler(Math.PI / 2.0, 0.0, 0.0)
-                    : message.props.plane == "yx"
-                      ? new THREE.Euler(0.0, Math.PI / 2.0, Math.PI / 2.0)
-                      : message.props.plane == "yz"
-                        ? new THREE.Euler(0.0, 0.0, Math.PI / 2.0)
-                        : message.props.plane == "zx"
-                          ? new THREE.Euler(0.0, Math.PI / 2.0, 0.0)
-                          : message.props.plane == "zy"
-                            ? new THREE.Euler(
-                                -Math.PI / 2.0,
-                                0.0,
-                                -Math.PI / 2.0,
-                              )
-                            : undefined
+                  ? new THREE.Euler(Math.PI / 2.0, 0.0, 0.0)
+                  : message.props.plane == "yx"
+                  ? new THREE.Euler(0.0, Math.PI / 2.0, Math.PI / 2.0)
+                  : message.props.plane == "yz"
+                  ? new THREE.Euler(0.0, 0.0, Math.PI / 2.0)
+                  : message.props.plane == "zx"
+                  ? new THREE.Euler(0.0, Math.PI / 2.0, 0.0)
+                  : message.props.plane == "zy"
+                  ? new THREE.Euler(-Math.PI / 2.0, 0.0, -Math.PI / 2.0)
+                  : undefined
               }
             />
           </group>
