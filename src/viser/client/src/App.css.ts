ef6887b4be177ca6a1ef232f41e800e1a93dbdaf
brentyi <yibrenth@gmail.com>
2024-10-13 23:31:42 -0700

    Mobile size adjustments, comments fixes

--- src/viser/client/src/App.css.ts
+++ src/viser/client/src/App.css.ts
@@ -3,3 +3,12 @@ import { globalStyle } from "@vanilla-extract/css";
 globalStyle(".mantine-ScrollArea-scrollbar", {
   zIndex: 100,
 });
+
+globalStyle("html", {
+  fontSize: "100%",
+  "@media": {
+    "(max-width: 767px)": {
+      fontSize: "85%",
+    },
+  },
+});
