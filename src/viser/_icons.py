098b602232b64eeb1add6611d4c353d40850d7a2
brentyi <yibrenth@gmail.com>
2024-10-14 20:38:29 -0700

    Faster icons

--- src/viser/_icons.py
+++ src/viser/_icons.py
@@ -1,4 +1,5 @@
-import tarfile
+import zipfile
+from functools import lru_cache
 from pathlib import Path
 
 from ._icons_enum import IconName
@@ -6,15 +7,15 @@ from ._icons_enum import IconName
 ICONS_DIR = Path(__file__).absolute().parent / "_icons"
 
 
+@lru_cache(maxsize=32)
 def svg_from_icon(icon_name: IconName) -> str:
     """Read an icon and return it as a UTF string; we expect this to be an
     <svg /> tag."""
     assert isinstance(icon_name, str)
-    icons_tarball = ICONS_DIR / "tabler-icons.tar"
+    icons_zipfile = ICONS_DIR / "tabler-icons.zip"
 
-    with tarfile.open(icons_tarball) as tar:
-        icon_file = tar.extractfile(f"{icon_name}.svg")
-        assert icon_file is not None
-        out = icon_file.read()
+    with zipfile.ZipFile(icons_zipfile) as zip_file:
+        with zip_file.open(f"{icon_name}.svg") as icon_file:
+            out = icon_file.read()
 
     return out.decode("utf-8")
