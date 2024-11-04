a1bf279348732bba423ad5f6652adc3d23964f61
brentyi <yibrenth@gmail.com>
2024-11-04 01:03:37 -0800

    Add sleep to server stop test

--- tests/test_server_stop.py
+++ tests/test_server_stop.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 import viser
 import viser._client_autobuild
@@ -16,8 +17,9 @@ def test_server_port_is_freed():
     result = sock.connect_ex(("localhost", original_port))
     assert result == 0
     sock.close()
-
     server.stop()
+
+    time.sleep(0.05)
 
     # Assert that the port is now free.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
