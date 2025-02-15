31cc8282f2341f927dd143887780ec6b7a987050
brentyi <yibrenth@gmail.com>
2025-02-15 11:43:27 -0800

    Add shadow args tsx

---
+++ src/viser/client/src/ShadowArgs.tsx
@@ -0,0 +1,7 @@
+export const shadowArgs = {
+  "shadow-camera-near": 0.001, // Near plane
+  "shadow-bias": -0.0001,
+  "shadow-mapSize-width": 1024,
+  "shadow-mapSize-height": 1024,
+  "shadow-radius": 4.0,
+};
