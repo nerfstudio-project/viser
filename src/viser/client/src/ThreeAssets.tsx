0ed850a91f0b756a0843d6c01457aca1fec91a94
brentyi <yibrenth@gmail.com>
2025-03-20 12:04:28 -0700

    Fix key error

--- src/viser/client/src/ThreeAssets.tsx
+++ src/viser/client/src/ThreeAssets.tsx
@@ -270,9 +270,11 @@ export const GlbAsset = React.forwardRef<
       {gltf === undefined ? null : (
         <>
           <primitive object={gltf.scene} scale={scale} />
-          {meshes.map((mesh) =>
-            createPortal(<OutlinesIfHovered alwaysMounted />, mesh),
-          )}
+          {meshes.map((mesh, i) => (
+            <React.Fragment key={i}>
+              {createPortal(<OutlinesIfHovered alwaysMounted />, mesh)}
+            </React.Fragment>
+          ))}
         </>
       )}
     </group>
