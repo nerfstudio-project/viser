e3ea31460d99076b8c44ea00f1b5b69dd6ebddfa
brentyi <yibrenth@gmail.com>
2025-03-28 23:18:07 -0700

    Add `precision=` argument to point clouds

--- src/viser/_messages.py
+++ src/viser/_messages.py
@@ -387,7 +387,7 @@ class PointCloudMessage(_CreateSceneNodeMessage):
 
 @dataclasses.dataclass
 class PointCloudProps:
-    points: npt.NDArray[np.float32]
+    points: npt.NDArray[np.float16] | npt.NDArray[np.float32]
     """Location of points. Should have shape (N, 3). Synchronized automatically when assigned."""
     colors: npt.NDArray[np.uint8]
     """Colors of points. Should have shape (N, 3) or (3,). Synchronized automatically when assigned."""
@@ -395,6 +395,10 @@ class PointCloudProps:
     """Size of each point. Synchronized automatically when assigned."""
     point_ball_norm: float
     """Norm value determining the shape of each point. Synchronized automatically when assigned."""
+    precision: Literal["fp16", "fp32"]
+    """Precision of the point cloud. Assignments to `points` are automatically casted
+    based on the current precision value. Updates to `points` should therefore happen
+    *after* updates to `precision`. Synchronized automatically when assigned."""
 
     def __post_init__(self):
         # Check shapes.
@@ -402,7 +406,10 @@ class PointCloudProps:
         assert self.points.shape[-1] == 3
 
         # Check dtypes.
-        assert self.points.dtype == np.float32
+        if self.precision == "fp16":
+            assert self.points.dtype == np.float16
+        else:
+            assert self.points.dtype == np.float32
         assert self.colors.dtype == np.uint8
 
 
