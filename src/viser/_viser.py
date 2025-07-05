76856811f8b4ccf61fa7631856f4690920d8fabe
brentyi <yibrenth@gmail.com>
2025-07-04 21:37:51 -0700

    Update docs with deprecation note

--- src/viser/_viser.py
+++ src/viser/_viser.py
@@ -492,6 +492,9 @@ class ClientHandle(_BackwardsCompatibilityShim if not TYPE_CHECKING else object)
         top left corner of the client's viewer. Notifications are useful for
         providing alerts or status updates to users.
 
+        .. deprecated::
+            The `auto_close` argument is deprecated. Use `auto_close_seconds` instead.
+
         Args:
             title: Title to display on the notification.
             body: Message to display on the notification body.
