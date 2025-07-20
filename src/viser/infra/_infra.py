097410bcbfd19a71956e8bd8092f4c43815cd88b
brentyi <yibrenth@gmail.com>
2025-07-19 20:39:25 -0700

    Fix URL

--- src/viser/infra/_infra.py
+++ src/viser/infra/_infra.py
@@ -274,7 +274,7 @@ class WebsockServer(WebsockMessageHandler):
         # Exit the server thread when the main process exits. This would happen
         # automatically, but is nice to do explicitly to avoid some nanobind
         # reference leak warnings:
-        # https://github.com/inducer/pyopencl/issues/758
+        # https://github.com/nerfstudio-project/viser/issues/518
         atexit.register(self.stop)
 
         # Broadcast buffer should be populated by the background worker.
