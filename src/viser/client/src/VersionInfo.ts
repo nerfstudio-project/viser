553403a371640cbc53f6771be2827d3a333d267c
brentyi <yibrenth@gmail.com>
2025-09-05 17:44:30 -0700

    `1.0.8`

--- src/viser/client/src/VersionInfo.ts
+++ src/viser/client/src/VersionInfo.ts
@@ -1,6 +1,6 @@
 // Automatically generated file - do not edit manually.
 // This is synchronized with the Python package version in viser/__init__.py.
-export const VISER_VERSION = "1.0.7";
+export const VISER_VERSION = "1.0.8";
 
 // GitHub contributors for the viser project.
 export interface Contributor {
