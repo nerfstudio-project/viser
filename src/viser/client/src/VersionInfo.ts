4bc3f801dc6da99ecfc9a1d17a060ae61542e33a
brentyi <yibrenth@gmail.com>
2025-10-13 01:43:43 -0700

    `1.0.15`

--- src/viser/client/src/VersionInfo.ts
+++ src/viser/client/src/VersionInfo.ts
@@ -1,6 +1,6 @@
 // Automatically generated file - do not edit manually.
 // This is synchronized with the Python package version in viser/__init__.py.
-export const VISER_VERSION = "1.0.14";
+export const VISER_VERSION = "1.0.15";
 
 // GitHub contributors for the viser project.
 export interface Contributor {
